7b8a68e7fc421e0da932fcebc765d989f484a868
abaferas <wasfyelgamed@gmail.com>
2023-07-27 04:23:48 +0300

    add gradle entities dependencies

--- entities/build.gradle.kts
+++ entities/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
