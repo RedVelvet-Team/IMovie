8e117c1c0286e912241c21e95c1a37048843be3b
abaferas <wasfyelgamed@gmail.com>
2023-07-27 20:44:32 +0300

    add gson dependencies

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
 
+    implementation("com.google.code.gson:gson:2.10.1")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
 kapt {
