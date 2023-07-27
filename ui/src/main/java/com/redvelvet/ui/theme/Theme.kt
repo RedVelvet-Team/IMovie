d4dffac8ea9bfcbb77a436257db883c0974cd5de
Haidy <haidyabugom3a@gmail.com>
2023-07-27 17:40:02 +0300

    add colors

--- ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
+++ ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
@@ -17,24 +17,16 @@ import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    secondary = Secondary,
+    background = Primary,
+    onSecondary = OnSecondary,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    secondary = Secondary,
+    background = Primary,
+    onSecondary = OnSecondary,
 )
 
 @Composable
