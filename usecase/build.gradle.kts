57aa35d77371af129758d9f350e56a0cafd859c5
abaferas <wasfyelgamed@gmail.com>
2023-07-27 04:24:44 +0300

    add gradle usecase dependencies

--- usecase/build.gradle.kts
+++ usecase/build.gradle.kts
@@ -2,8 +2,14 @@ plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
 }
-
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+dependencies{
+    implementation(project(":entities"))
 }
