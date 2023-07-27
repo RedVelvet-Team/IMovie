224cc60329c810a00dab78c3fb0396e8043cdbd8
Haidy <haidyabugom3a@gmail.com>
2023-07-27 20:02:09 +0300

    finish theme

--- ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
+++ ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
@@ -17,7 +17,7 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Primary,
     secondary = Secondary,
     background = Primary,
     onSecondary = OnSecondary,
