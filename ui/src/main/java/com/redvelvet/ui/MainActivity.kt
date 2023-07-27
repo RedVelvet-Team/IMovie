a0272fa2b529814e218e9731bdc866f2cbc9079b
abaferas <wasfyelgamed@gmail.com>
2023-07-27 04:22:06 +0300

    add test function in main activity

--- ui/src/main/java/com/redvelvet/ui/MainActivity.kt
+++ ui/src/main/java/com/redvelvet/ui/MainActivity.kt
@@ -1,17 +1,20 @@
-package com.redvelvet.imovie
+package com.redvelvet.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.redvelvet.imovie.ui.theme.IMovieTheme
+import com.redvelvet.ui.theme.IMovieTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +24,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //Add code here
+                    Column(modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(text = "Hello Movie App")
+                    }
                 }
             }
         }
