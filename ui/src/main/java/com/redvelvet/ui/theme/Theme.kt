9616bbd2d985125f8d4466f08ed116ee85114e89
Haidy <haidyabugom3a@gmail.com>
2023-07-27 19:17:05 +0300

    finish theme

--- ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
+++ ui/src/main/java/com/redvelvet/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -54,9 +55,16 @@ fun IMovieTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalSpacing provides Spacing(),
+        LocalRadius provides Radius(),
+        LocalDimens provides Dimens()
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
+
 }
