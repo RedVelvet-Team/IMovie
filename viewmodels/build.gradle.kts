9e30f64a475250f78c857c3dd86f59d2f8c1df8f
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-07-29 21:58:39 +0300

    fix issues

--- viewmodels/build.gradle.kts
+++ viewmodels/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 
 dependencies {
     implementation(project(":usecase"))
-    implementation(project(":entities"))
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
