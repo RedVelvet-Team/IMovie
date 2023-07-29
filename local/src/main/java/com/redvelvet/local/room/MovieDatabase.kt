993cdd69ea234089e8477c64fc38b43172eaa7e8
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-07-29 05:28:31 +0300

    add di

--- local/src/main/java/com/redvelvet/local/room/MovieDatabase.kt
+++ local/src/main/java/com/redvelvet/local/room/MovieDatabase.kt
@@ -2,9 +2,10 @@ package com.redvelvet.local.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.redvelvet.local.entity.MovieEntity
 
 
-@Database(entities = [], version = 1)
+@Database(entities = [MovieEntity::class], version = 1)
 abstract class MovieDatabase : RoomDatabase() {
     abstract fun getMovieDao(): MovieDao
 }
