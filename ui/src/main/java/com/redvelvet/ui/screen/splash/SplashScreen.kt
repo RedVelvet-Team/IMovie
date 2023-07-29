3c59654a2f84ceb2765bc88f7ffe29331cbf3ada
Haidy <haidyabugom3a@gmail.com>
2023-07-29 18:40:29 +0300

    finish spalsh screen

--- ui/src/main/java/com/redvelvet/ui/screen/splash/SplashScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/splash/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.redvelvet.ui.screens.splash
+package com.redvelvet.ui.screen.splash
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
@@ -16,35 +16,59 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.redvelvet.ui.R
+import com.redvelvet.ui.navigation.MovieDestination
 import com.redvelvet.ui.theme.Primary
+import com.redvelvet.viewmodel.SplashUiState
+import com.redvelvet.viewmodel.SplashViewModel
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen(viewModel: SplashViewModel = hiltViewModel()) {
+fun SplashScreen(navController: NavController, viewModel: SplashViewModel = hiltViewModel()) {
     val state by viewModel.state.collectAsState()
-    SplashContent(state)
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(Primary, darkIcons = false)
+    val event = object : SplashUiEvent {
+        override fun navigateToHome() {
+            navigateTo(navController,MovieDestination.Home.route)
+        }
+
+        override fun navigateToOnBoarding() {
+            navigateTo(navController,MovieDestination.OnBoarding.route)
+        }
+
+        override fun navigateToLogin() {
+            navigateTo(navController,MovieDestination.Login.route)
+        }
+    }
+
+    SplashContent(state, event)
 }
 
 @Composable
-fun SplashContent(
-    state: SplashUiState
+private fun SplashContent(
+    state: SplashUiState,
+    event: SplashUiEvent
 ) {
-    val systemUiController = rememberSystemUiController()
-    systemUiController.setSystemBarsColor(Primary, darkIcons = false)
     val rotationDegree = remember { Animatable(0f) }
-    LaunchedEffect(key1 = state) {
+    LaunchedEffect(key1 = Unit) {
         rotationDegree.animateTo(
             targetValue = 360f,
             animationSpec = tween(durationMillis = 850)
         )
-        delay(1000)
-//        if (!state.isLogged) {
+        delay(1500)
+        if (state.isFirstTimeUseApp) {
+            event.navigateToOnBoarding()
+            return@LaunchedEffect
+        }
+        if (state.isLogged || state.isGuest) {
+            event.navigateToHome()
+            return@LaunchedEffect
+        }
+        event.navigateToLogin()
 
-//        } else {
-
-//        }
     }
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -59,8 +83,10 @@ fun SplashContent(
     }
 }
 
-//@Composable
-//@Preview(showSystemUi = true)
-//fun previewSplash() {
-//    SplashScreen()
-//}
+private fun navigateTo(navController: NavController,route: String){
+    navController.navigate(route) {
+        popUpTo(MovieDestination.Splash.route) {
+            inclusive = true
+        }
+    }
+}
