00d596ec215e8c64b878fad4cdb73db21a1a4840
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-07-29 21:31:40 +0300

    fix issues

--- usecase/src/main/java/com/redvelvet/usecase/usecase/user/SetUserNotFirstTimeUseAppUseCaseImpl.kt
+++ usecase/src/main/java/com/redvelvet/usecase/usecase/user/SetUserNotFirstTimeUseAppUseCaseImpl.kt
@@ -4,7 +4,7 @@ import com.redvelvet.usecase.repository.UserRepository
 import javax.inject.Inject
 
 class SetUserNotFirstTimeUseAppUseCaseImpl @Inject constructor(private val userRepository: UserRepository) {
-    suspend fun invoke() {
-        userRepository.setIsFirstTimeUsingApp(false)
+    suspend fun invoke(isFirstTime: Boolean) {
+        userRepository.setIsFirstTimeUsingApp(isFirstTime)
     }
 }
