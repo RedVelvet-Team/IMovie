f088a4c613ed285404c5d230dab7e6f64c18fff9
HassanAyman <hassan3ayman@gmail.com>
2023-07-30 21:55:52 +0300

    handle imported issues

--- viewmodels/src/main/java/com/redvelvet/viewmodel/base/BaseViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/base/BaseViewModel.kt
@@ -11,8 +11,8 @@ import kotlinx.coroutines.launch
 
 abstract class BaseViewModel<UiState : BaseUiState>(state: UiState) : ViewModel() {
 
-    private val _state = MutableStateFlow(state)
-    protected val state = _state.asStateFlow()
+    protected val _state = MutableStateFlow(state)
+    val state = _state.asStateFlow()
 
     fun <T> tryToExecute(
         function: suspend () -> T,
