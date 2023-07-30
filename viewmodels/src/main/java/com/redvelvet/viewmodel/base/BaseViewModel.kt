c9d3ba342f18ff658129a2df0abf4b58dec07010
HassanAyman <hassan3ayman@gmail.com>
2023-07-30 18:39:15 +0300

    make ErrorUiState and mapper from ErrorType to it and tryToExceute function

---
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/base/BaseViewModel.kt
@@ -0,0 +1,34 @@
+package com.redvelvet.viewmodel.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.redvelvet.entities.ErrorType
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<UiState : BaseUiState>(state: UiState) : ViewModel() {
+
+    private val _state = MutableStateFlow(state)
+    protected val state = _state.asStateFlow()
+
+    fun <T> tryToExecute(
+        function: suspend () -> T,
+        onSuccess: (T) -> Unit,
+        onError: (error: ErrorUiState) -> Unit,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO
+    ) {
+        viewModelScope.launch(dispatcher) {
+            try {
+                val result = function()
+                onSuccess(result)
+            } catch (e: ErrorType) {
+                onError(e.toErrorUiState())
+            }
+        }
+    }
+}
+
+interface BaseUiState
