9181ce5804eb54002a390028c9478b5d911306fe
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-01 00:09:44 +0300

    improve compose function of custom top app bar

--- ui/src/main/java/com/redvelvet/ui/composable/CustomTopAppBar.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/CustomTopAppBar.kt
@@ -1,33 +1,40 @@
 package com.redvelvet.ui.composable
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.redvelvet.ui.theme.Primary
 import com.redvelvet.ui.theme.Typography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CustomTopAppBar(title: String,hasBackArrow: Boolean) {
-    CenterAlignedTopAppBar(
+fun CustomTopAppBar(title: String, hasBackArrow: Boolean, modifier: Modifier = Modifier) {
+    TopAppBar(
         title = {
             Text(
+                modifier = modifier,
                 text = title,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center,
                 style = Typography.headlineLarge
             )
         },
         navigationIcon = {
-            when(hasBackArrow){
+            when (hasBackArrow) {
                 true -> {
                     IconButton(onClick = { /* doSomething() */ }) {
                         Icon(
@@ -42,6 +49,9 @@ fun CustomTopAppBar(title: String,hasBackArrow: Boolean) {
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Primary,
             titleContentColor = Color.White
-        )
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(16.dp)
     )
 }
