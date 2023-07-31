061a62f0932973ee5e459e1f923b05bbb19942aa
HassanAyman <hassan3ayman@gmail.com>
2023-07-31 16:57:34 +0300

    convert errorFunction ti hashmap

--- remote/src/main/java/com/redvelvet/remote/source/RemoteDataSourceImp.kt
+++ remote/src/main/java/com/redvelvet/remote/source/RemoteDataSourceImp.kt
@@ -13,45 +13,47 @@ class RemoteDataSourceImp @Inject constructor(
 
 
 
+
     private suspend fun <T> wrapResponse(
         function: suspend () -> Response<ErrorResponse<T>>
     ): T {
         try {
             val response = function()
-            val responseBody = response.body()?: throw RemoteError.NullData
+            val responseBody = response.body() ?: throw RemoteError.NullData
             if (response.isSuccessful) {
                 return responseBody.value ?: throw RemoteError.NullData
             } else {
-                throw remoteErrorException(responseBody.code ?: 0)
+                throw remoteErrorMap[responseBody.code ?: 0]!!
             }
         }catch (e: Exception){
             throw RemoteError.Network
         }
     }
 
-    private fun remoteErrorException(errorCode: Int) = when (errorCode) {
-        3 -> RemoteError.AuthenticationFailed
-        7 -> RemoteError.InvalidApiKey
-        9 -> RemoteError.ServiceOffline
-        10 -> RemoteError.SuspendedApiKey
-        11 -> RemoteError.InternalError
-        14 -> RemoteError.AuthenticationFailed2
-        15 -> RemoteError.Failed
-        16 -> RemoteError.DeviceDenied
-        17 -> RemoteError.SessionDenied
-        24 -> RemoteError.BackendTimeout
-        26 -> RemoteError.UsernamePasswordRequired
-        30 -> RemoteError.InvalidUsernamePassword
-        31 -> RemoteError.AccountDisabled
-        32 -> RemoteError.EmailNotVerified
-        33 -> RemoteError.InvalidRequestToken
-        35 -> RemoteError.InvalidToken
-        36 -> RemoteError.InsufficientPermissions
-        39 -> RemoteError.ResourcePrivate
-        43 -> RemoteError.ConnectionError
-        44 -> RemoteError.InvalidId
-        46 -> RemoteError.MaintenanceMode
-        47 -> RemoteError.InvalidInput
-        else -> RemoteError.InternalError
-    }
+   private companion object RemoteErrorMap{
+       private val remoteErrorMap = mapOf(
+           3 to RemoteError.AuthenticationFailed,
+           7 to RemoteError.InvalidApiKey,
+           9 to RemoteError.ServiceOffline,
+           10 to RemoteError.SuspendedApiKey,
+           11 to RemoteError.InternalError,
+           14 to RemoteError.AuthenticationFailed2,
+           15 to RemoteError.Failed,
+           16 to RemoteError.DeviceDenied,
+           17 to RemoteError.SessionDenied,
+           24 to RemoteError.BackendTimeout,
+           26 to RemoteError.UsernamePasswordRequired,
+           30 to RemoteError.InvalidUsernamePassword,
+           31 to RemoteError.AccountDisabled,
+           32 to RemoteError.EmailNotVerified,
+           33 to RemoteError.InvalidRequestToken,
+           35 to RemoteError.InvalidToken,
+           36 to RemoteError.InsufficientPermissions,
+           39 to RemoteError.ResourcePrivate,
+           43 to RemoteError.ConnectionError,
+           44 to RemoteError.InvalidId,
+           46 to RemoteError.MaintenanceMode,
+           47 to RemoteError.InvalidInput
+       ).withDefault { RemoteError.InternalError }
+   }
 }
