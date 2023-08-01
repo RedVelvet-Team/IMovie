91ec4c3a125349ad344f1a385e65f996bd1b2c9a
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-02 00:02:57 +0300

    add media details background

---
+++ ui/src/main/java/com/redvelvet/ui/composable/BackgroundMediaImage.kt
@@ -0,0 +1,26 @@
+package com.redvelvet.ui.composable
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Scale
+
+@Composable
+fun BackgroundMediaImage(
+    image: String,
+    placeholder: Int,
+    contentDescription: Int,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        modifier = modifier,
+        model = ImageRequest.Builder(LocalContext.current).data(image)
+            .crossfade(true).scale(Scale.FILL).build(),
+        placeholder = painterResource(placeholder),
+        contentDescription = stringResource(contentDescription),
+    )
+}
---
+++ ui/src/main/java/com/redvelvet/ui/screen/movieDetails/mediaComposable/MediaDetailsBackgroundContent.kt
@@ -0,0 +1,51 @@
+package com.redvelvet.ui.screen.movieDetails.mediaComposable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.redvelvet.ui.R
+import com.redvelvet.ui.composable.BackgroundMediaImage
+import com.redvelvet.ui.composable.PlayMedia
+import com.redvelvet.ui.screen.movieDetails.MovieDetailsScreen
+import com.redvelvet.ui.theme.spacing
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    device = "spec:orientation=portrait,width=800dp,height=360dp"
+)
+@Composable
+fun PreviewMovieDetailsScreen() {
+    MovieDetailsScreen(rememberNavController())
+}
+
+@Composable
+fun MediaDetailsBackgroundContent(
+    mediaImage: String,
+    onMediaPlay: () -> Unit
+) {
+    Box {
+        BackgroundMediaImage(
+            image = mediaImage,
+            placeholder = R.drawable.ic_launcher_background,
+            contentDescription = R.string.movies_details_background_image,
+            modifier = Modifier.fillMaxWidth()
+        )
+        PlayMedia(
+            icon = R.drawable.play_media,
+            description = R.string.movies_details_play,
+            onMediaPlay = onMediaPlay,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = MaterialTheme.spacing.spacing127
+                )
+        )
+    }
+}
