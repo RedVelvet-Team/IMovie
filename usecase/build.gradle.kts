0b10bb11a139b84998292c45c372cd1ff8a19f60
abaferas <wasfyelgamed@gmail.com>
2023-08-02 06:12:00 +0300

    add courotine dependencies in usecase module

--- usecase/build.gradle.kts
+++ usecase/build.gradle.kts
@@ -16,4 +16,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.dagger:dagger:2.46.1")
     testImplementation("org.testng:testng:6.9.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
+
