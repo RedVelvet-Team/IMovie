5075e1defccce603a7d9a60a69bce8037acbf541
HassanAyman <hassan3ayman@gmail.com>
2023-08-02 16:45:14 +0300

    make search use case

---
+++ usecase/src/main/java/com/redvelvet/usecase/usecase/search/GetSearchResultUseCase.kt
@@ -0,0 +1,13 @@
+package com.redvelvet.usecase.usecase.search
+
+import com.redvelvet.entities.search.SearchResult
+import com.redvelvet.usecase.repository.MovieRepository
+import javax.inject.Inject
+
+class GetSearchResultUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(query: String): List<SearchResult>{
+        return movieRepository.multiSearch(query)
+    }
+}
