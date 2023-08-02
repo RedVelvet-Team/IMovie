133e4e3d4e79ce31c1c5deb5cc320eb2035a4633
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 01:17:03 +0300

    complete movie details view model

--- viewmodels/src/main/java/com/redvelvet/viewmodel/movieDetails/MovieDetailsViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/movieDetails/MovieDetailsViewModel.kt
@@ -4,11 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.redvelvet.entities.error.ErrorType
 import com.redvelvet.usecase.usecase.movie.GetMovieFullDetailsUseCase
-import com.redvelvet.viewmodel.base.ErrorUiState
-import com.redvelvet.viewmodel.base.toErrorUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -20,7 +18,8 @@ class MovieDetailsViewModel @Inject constructor(
     private val getMovieFullDetailsUseCase: GetMovieFullDetailsUseCase
 ) : ViewModel() {
 
-    private val _state: MutableStateFlow<MovieDetailsUiState> = MutableStateFlow(MovieDetailsUiState())
+    private val _state: MutableStateFlow<MovieDetailsScreenUiState> =
+        MutableStateFlow(MovieDetailsScreenUiState())
     val state = _state.asStateFlow()
 
     init {
@@ -28,182 +27,72 @@ class MovieDetailsViewModel @Inject constructor(
     }
 
     private fun getData() {
-        viewModelScope.launch(Dispatchers.IO) {
+        _state.update { MovieDetailsScreenUiState(isLoading = true, isError = Pair(false, "")) }
+        viewModelScope.launch {
+        delay(2000)
             try {
+//                _state.update {
+//                    MovieDetailsScreenUiState(
+//                        data = getMovieFullDetailsUseCase(5585).toMovieFullDetailsScreenUiState(),
+//                        isLoading = false
+//                    )
+//                }
+
                 _state.update {
-                    MovieDetailsUiState(data = listOf(getMovieFullDetailsUseCase(5585)),isLoading = false)
+                    MovieDetailsScreenUiState(data = fakeData(), isLoading = false)
                 }
+
+
             } catch (e: ErrorType) {
                 _state.update {
-                    MovieDetailsUiState(data = emptyList(),isLoading = false, isError = Pair(true,e.message.toString()))
+                    MovieDetailsScreenUiState(
+                        data = null,
+                        isLoading = false,
+                        isError = Pair(true, e.message.toString())
+                    )
                 }
             }
         }
     }
 
-    /*private fun fakeData() {
-        _state.update {
-            it.copy(
-                movieDetails = MovieDetailsScreenUiState.MovieDetailsUiState(
-                    movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    movieName = "Real Madrid",
-                    listOfGenres = listOf("Action", "Drama"),
-                    movieTime = "2h 30m",
-                    languageOfMovie = "English",
-                    movieRate = "8.5",
-                    movieDescription = "This is a movie about Real Madrid",
-                    movieStatus = "Released",
-                    movieReleaseDate = "23/8/2001",
-                    movieProductionCountries = "Espania",
-                ),
-                movieTopCasts = listOf(
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.TopCastUiState(
-                        castImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        castName = "Real Madrid",
-                    ),
-
-                    ),
-                movieKeywords = listOf(
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-                    MovieDetailsScreenUiState.KeywordUiState(
-                        keywordName = "Real Madrid",
-                        keywordId = 1,
-                    ),
-
-                    ),
-                similarMovies = listOf(
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                ),
-                movieImages = listOf(
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                    "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                ),
-                movieReviews = listOf(
-                    MovieDetailsScreenUiState.MovieReviewUiState(
-                        reviewAuthor = "Real Madrid",
-                        reviewDescription = "Real Madrid",
-                        reviewRate = "4.5",
-                        reviewDate = "23/8/2001",
-                    ),
-                    MovieDetailsScreenUiState.MovieReviewUiState(
-                        reviewAuthor = "Real Madrid",
-                        reviewDescription = "Real Madrid dcpdokk podvd",
-                        reviewRate = "4.5",
-                        reviewDate = "23/8/2001",
-                    ),
-
-                    MovieDetailsScreenUiState.MovieReviewUiState(
-                        reviewAuthor = "Real Madrid",
-                        reviewDescription = "Real Madrid dcpdokk podvd",
-                        reviewRate = "4.5",
-                        reviewDate = "23/8/2001",
-                    ),
-                ),
-                recommendedMovies = listOf(
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                    MovieDetailsScreenUiState.MovieUiState(
-                        movieImage = "https://www.realmadrid.com/StaticFiles/RealMadridResponsive/images/static/twitter-image.png",
-                        movieName = "Real Madrid",
-                    ),
-                ),
+    private fun fakeData(): MovieDetailsScreenUiState.MovieFullDetailsUiState {
+        return MovieDetailsScreenUiState.MovieFullDetailsUiState(
+            details = MovieDetailsScreenUiState.MovieDetailsUiState(
+                genres = listOf("Action", "Adventure", "Fantasy"),
+                homepage = "https://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
+                id = 11,
+                originalTitle = "Star Wars",
+                overview = "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+                posterPath = "https://image.tmdb.org/t/p/w500/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg",
+                productionCountries = listOf("United States of America"),
+                releaseDate = "1977-05-25",
+                revenue = 775398007,
+                runtime = "121",
+                spokenLanguages = listOf("English"),
+                status = "Released",
+                tagline = "A long time ago in a galaxy far, far away...",
+                title = "Star Wars",
+                video = false,
+                voteAverage = 8.2
+            ),
+            images = MovieDetailsScreenUiState.MovieImagesUiState(
+                test = "test"
+            ),
+            keyWords = MovieDetailsScreenUiState.MovieKeyWordsUiState(
+                test = "test"
+            ),
+            recommendations = MovieDetailsScreenUiState.MovieRecommendationsUiState(
+                test = "test"
+            ),
+            reviews = MovieDetailsScreenUiState.MovieReviewsUiState(
+                test = "test"
+            ),
+            similar = MovieDetailsScreenUiState.MovieSimilarUiState(
+                test = "test"
+            ),
+            topCast = MovieDetailsScreenUiState.MovieTopCastUiState(
+                test = "test"
             )
-        }
-    }*/
-
-
-
+        )
+    }
 }
