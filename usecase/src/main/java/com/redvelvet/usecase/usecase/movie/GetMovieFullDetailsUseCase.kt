bd386f2a62b1bc21943f96d1ad827f9da919131a
abaferas <wasfyelgamed@gmail.com>
2023-08-02 06:12:22 +0300

    implement movie details usecases

---
+++ usecase/src/main/java/com/redvelvet/usecase/usecase/movie/GetMovieFullDetailsUseCase.kt
@@ -0,0 +1,38 @@
+package com.redvelvet.usecase.usecase.movie
+
+import com.redvelvet.entities.movie.details.MovieFullDetails
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class GetMovieFullDetailsUseCase @Inject private constructor(
+    private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
+    private val getMovieImagesUseCase: GetMovieImagesUseCase,
+    private val getMovieKeyWordUseCase: GetMovieKeyWordUseCase,
+    private val getMovieRecommendationUseCase: GetMovieRecommendationUseCase,
+    private val getMovieReviewUseCase: GetMovieReviewUseCase,
+    private val getMovieSimilarUseCase: GetMovieSimilarUseCase,
+    private val getMovieTopCastUseCase: GetMovieTopCastUseCase
+) {
+    suspend operator fun invoke(movieId: Int): MovieFullDetails =
+        coroutineScope {
+            val details = getMovieDetailsUseCase(movieId)
+            val images = getMovieImagesUseCase(movieId)
+            val keyWords = getMovieKeyWordUseCase(movieId)
+            val recommendations = getMovieRecommendationUseCase(movieId)
+            val reviews = getMovieReviewUseCase(movieId)
+            val similar = getMovieSimilarUseCase(movieId)
+            val topCast = getMovieTopCastUseCase(movieId)
+
+            MovieFullDetails(
+                details = details,
+                images = images,
+                keyWords = keyWords,
+                recommendations = recommendations,
+                reviews = reviews,
+                similar = similar,
+                topCast = topCast
+            )
+    }
+}
+
