6b3a3b2ed71914dfe74fedc87e30ca33ac42a517
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 01:17:14 +0300

    Update MovieDetailsRoute.kt

--- ui/src/main/java/com/redvelvet/ui/screen/movieDetails/MovieDetailsRoute.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/movieDetails/MovieDetailsRoute.kt
@@ -6,9 +6,73 @@ import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.composable
 import com.redvelvet.ui.navigation.MovieDestination
 
+class MovieEventsTest : MovieDetailsUiEvent {
+    override fun onMovieFavorite(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieSave(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBack() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPlayTrailer() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieCategory(category: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTopCastSeeAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCast(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieKeyword(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSimilarMovieSeeAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRecommendedMovieSeeAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovie(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieImagesSeeAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieImage(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieReviewsSeeAll() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMovieReview(id: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+
 fun NavGraphBuilder.movieDetailsRoute(navController: NavController) {
     composable(route = MovieDestination.MovieDetails.route) {
-        MovieDetailsScreen(navController = navController)
+        val uiEvent: MovieDetailsUiEvent = MovieEventsTest()
+        MovieDetailsScreen(navController = navController, uiEvent)
     }
 }
 
