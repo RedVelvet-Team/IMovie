fc5373e9581fb35727d7eaca84a2991365273dca
abaferas <wasfyelgamed@gmail.com>
2023-08-02 05:06:29 +0300

    implement mapper MovieReviews

--- entities/src/main/java/com/redvelvet/entities/movie/details/MovieReviews.kt
+++ entities/src/main/java/com/redvelvet/entities/movie/details/MovieReviews.kt
@@ -2,10 +2,15 @@ package com.redvelvet.entities.movie.details
 
 data class MovieReviews(
     val id: Int,
-    val name: String,
-    val username: String,
-    val content: String,
-    val createdAt: String,
-    val url: String,
-    val rating: Double
-)
+    val results:List<Result>
+){
+    data class Result(
+        val id: String,
+        val name: String,
+        val username: String,
+        val content: String,
+        val createdAt: String,
+        val url: String,
+        val rating: Double
+    )
+}
--- repository/src/main/java/com/redvelvet/repository/mapper/MovieDetailsMapper.kt
+++ repository/src/main/java/com/redvelvet/repository/mapper/MovieDetailsMapper.kt
@@ -3,28 +3,44 @@ package com.redvelvet.repository.mapper
 import com.redvelvet.entities.movie.details.MovieImages
 import com.redvelvet.entities.movie.details.MovieKeyWords
 import com.redvelvet.entities.movie.details.MovieRecommendations
+import com.redvelvet.entities.movie.details.MovieReviews
 import com.redvelvet.repository.dto.movie.details.MovieImagesDTO
 import com.redvelvet.repository.dto.movie.details.MovieKeyWordsDTO
 import com.redvelvet.repository.dto.movie.details.MovieRecommendationsDTO
+import com.redvelvet.repository.dto.movie.details.MovieReviewsDTO
 
-fun MovieImagesDTO.toDomain(): MovieImages{
-    return MovieImages(
-        backdrops = this.backdrops.map { MovieImages.Backdrop(it.filePath) },
+fun MovieImagesDTO.toDomain(): MovieImages {
+    return MovieImages(backdrops = this.backdrops.map { MovieImages.Backdrop(it.filePath) },
         id = this.id,
-        logos = this.logos.map { MovieImages.Logo(it.filePath)},
-        posters = this.posters.map { MovieImages.Poster(it.filePath) }
-    )
+        logos = this.logos.map { MovieImages.Logo(it.filePath) },
+        posters = this.posters.map { MovieImages.Poster(it.filePath) })
 }
 
-fun MovieKeyWordsDTO.toDomain(): MovieKeyWords{
-    return MovieKeyWords(
-        id = this.id,
-        keywords = this.keywords.map { MovieKeyWords.Keyword(it.id,it.name) }
-    )
+fun MovieKeyWordsDTO.toDomain(): MovieKeyWords {
+    return MovieKeyWords(id = this.id,
+        keywords = this.keywords.map { MovieKeyWords.Keyword(it.id, it.name) })
 }
 
-fun MovieRecommendationsDTO.toDomain(): MovieRecommendations{
-    return MovieRecommendations(
-        results = this.results.map { MovieRecommendations.Result(it.id,it.title,it.posterPath) }
-    )
+fun MovieRecommendationsDTO.toDomain(): MovieRecommendations {
+    return MovieRecommendations(results = this.results.map {
+        MovieRecommendations.Result(
+            it.id,
+            it.title,
+            it.posterPath
+        )
+    })
+}
+
+fun MovieReviewsDTO.toDomain(): MovieReviews {
+    return MovieReviews(id = this.id, results = this.results.map {
+        MovieReviews.Result(
+                id = it.id,
+                name = it.authorDetails.name,
+                username = it.authorDetails.username,
+                content = it.content,
+                createdAt = it.createdAt,
+                url = it.url,
+                rating = it.authorDetails.rating
+            )
+    })
 }
