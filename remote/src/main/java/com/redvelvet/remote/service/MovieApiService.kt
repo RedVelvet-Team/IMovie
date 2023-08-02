3b1cac03a9f6053e0b8e1c2a0f567742ccf17d03
abaferas <wasfyelgamed@gmail.com>
2023-08-02 04:03:44 +0300

    add movie details request functions

--- remote/src/main/java/com/redvelvet/remote/service/MovieApiService.kt
+++ remote/src/main/java/com/redvelvet/remote/service/MovieApiService.kt
@@ -5,6 +5,13 @@ import com.redvelvet.repository.dto.auth.GuestSessionDto
 import com.redvelvet.repository.dto.auth.LoginRequest
 import com.redvelvet.repository.dto.auth.SessionDto
 import com.redvelvet.repository.dto.auth.TokenDto
+import com.redvelvet.repository.dto.movie.MovieDto
+import com.redvelvet.repository.dto.movie.details.MovieImagesDTO
+import com.redvelvet.repository.dto.movie.details.MovieKeyWordsDTO
+import com.redvelvet.repository.dto.movie.details.MovieRecommendationsDTO
+import com.redvelvet.repository.dto.movie.details.MovieReviewsDTO
+import com.redvelvet.repository.dto.movie.details.MovieSimilarDTO
+import com.redvelvet.repository.dto.movie.details.MovieTopCastDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
@@ -12,6 +19,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface MovieApiService {
     @GET("authentication/token/ne")
@@ -31,4 +39,22 @@ interface MovieApiService {
     @FormUrlEncoded
     @DELETE("authentication/session")
     suspend fun deleteUserSession(@Field("session_id") sessionId: String): Response<DeleteSessionDto>
+
+
+    //region Movie
+    @GET("/movie/{movie_id}")
+    suspend fun getMovieDetailsById(@Path("movie_id") movieId: String): Response<MovieDto>
+    @GET("/movie/{movie_id}/images")
+    suspend fun getMovieImagesByID(@Path("movie_id") movieId: String):Response<MovieImagesDTO>
+    @GET("/movie/{movie_id}/keywords")
+    suspend fun getMovieKeyWordsByID(@Path("movie_id") movieId: String):Response<MovieKeyWordsDTO>
+    @GET("/movie/{movie_id}/recommendations")
+    suspend fun getMovieRecommendationsByID(@Path("movie_id") movieId: String):Response<MovieRecommendationsDTO>
+    @GET("/movie/{movie_id}/reviews")
+    suspend fun getMovieReviewsByID(@Path("movie_id") movieId: String):Response<MovieReviewsDTO>
+    @GET("/movie/{movie_id}/similar")
+    suspend fun getMovieSimilarByID(@Path("movie_id") movieId: String):Response<MovieSimilarDTO>
+    @GET("/movie/{movie_id}/credits")
+    suspend fun getMovieTopCastByID(@Path("movie_id") movieId: String):Response<MovieTopCastDto>
+    //endregion
 }
