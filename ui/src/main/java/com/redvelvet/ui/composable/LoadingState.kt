00e8e59d163f34059bfedb35e5badda77e652565
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 01:17:28 +0300

    Create LoadingState.kt

---
+++ ui/src/main/java/com/redvelvet/ui/composable/LoadingState.kt
@@ -0,0 +1,18 @@
+package com.redvelvet.ui.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoadingState() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
+    }
+}
