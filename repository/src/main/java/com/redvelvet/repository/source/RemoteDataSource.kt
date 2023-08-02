d9c4946cecb559d2af05a794f9e43433863eac16
abaferas <wasfyelgamed@gmail.com>
2023-08-02 04:07:57 +0300

    add movie remote data source functions implementation

--- repository/src/main/java/com/redvelvet/repository/source/RemoteDataSource.kt
+++ repository/src/main/java/com/redvelvet/repository/source/RemoteDataSource.kt
@@ -1,4 +1,26 @@
 package com.redvelvet.repository.source
 
+import com.redvelvet.repository.dto.movie.MovieDto
+import com.redvelvet.repository.dto.movie.details.MovieImagesDTO
+import com.redvelvet.repository.dto.movie.details.MovieKeyWordsDTO
+import com.redvelvet.repository.dto.movie.details.MovieRecommendationsDTO
+import com.redvelvet.repository.dto.movie.details.MovieReviewsDTO
+import com.redvelvet.repository.dto.movie.details.MovieSimilarDTO
+import com.redvelvet.repository.dto.movie.details.MovieTopCastDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface RemoteDataSource {
+
+    //region Movie Details
+    suspend fun getMovieDetailsById(@Path("movie_id") movieId: String): MovieDto
+    suspend fun getMovieImagesByID(@Path("movie_id") movieId: String):MovieImagesDTO
+    suspend fun getMovieKeyWordsByID(@Path("movie_id") movieId: String):MovieKeyWordsDTO
+    suspend fun getMovieRecommendationsByID(@Path("movie_id") movieId: String):MovieRecommendationsDTO
+    suspend fun getMovieReviewsByID(@Path("movie_id") movieId: String):MovieReviewsDTO
+    suspend fun getMovieSimilarByID(@Path("movie_id") movieId: String):MovieSimilarDTO
+    suspend fun getMovieTopCastByID(@Path("movie_id") movieId: String):MovieTopCastDto
+    //endregion
+
 }
