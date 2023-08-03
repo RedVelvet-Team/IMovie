22536b45d3d858fb526c842d4732c4da03c3017a
abaferas <wasfyelgamed@gmail.com>
2023-08-03 21:29:18 +0300

    implement MovieDetails ViewModel with valid response

--- viewmodels/src/main/java/com/redvelvet/viewmodel/movieDetails/MovieDetailsViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/movieDetails/MovieDetailsViewModel.kt
@@ -29,20 +29,13 @@ class MovieDetailsViewModel @Inject constructor(
     private fun getData() {
         _state.update { MovieDetailsScreenUiState(isLoading = true, isError = Pair(false, "")) }
         viewModelScope.launch {
-            delay(2000)
             try {
-//                _state.update {
-//                    MovieDetailsScreenUiState(
-//                        data = getMovieFullDetailsUseCase(5585).toMovieFullDetailsScreenUiState(),
-//                        isLoading = false
-//                    )
-//                }
-
                 _state.update {
-                    MovieDetailsScreenUiState(data = fakeData(), isLoading = false)
+                    MovieDetailsScreenUiState(
+                        data = getMovieFullDetailsUseCase(298618).toMovieFullDetailsScreenUiState(),
+                        isLoading = false
+                    )
                 }
-
-
             } catch (e: ErrorType) {
                 _state.update {
                     MovieDetailsScreenUiState(
@@ -53,70 +46,5 @@ class MovieDetailsViewModel @Inject constructor(
                 }
             }
         }
-    }
-
-    private fun fakeData(): MovieDetailsScreenUiState.MovieFullDetailsUiState {
-        return MovieDetailsScreenUiState.MovieFullDetailsUiState(
-            details = MovieDetailsScreenUiState.MovieDetailsUiState(
-                genres = listOf("Action", "Adventure", "Fantasy","Action", "Adventure", "Fantasy"),
-                homepage = "https://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
-                id = 11,
-                originalTitle = "Star Wars",
-                overview = "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
-                posterPath = "https://image.tmdb.org/t/p/w500/rktDFPbfHfUbArZ6OOOKsXcv0Bm.jpg",
-                productionCountries = listOf("United States of America"),
-                releaseDate = "1977-05-25",
-                revenue = 775398007,
-                runtime = "121",
-                spokenLanguages = "English, Arabic",
-                status = "Released",
-                tagline = "A long time ago in a galaxy far, far away...",
-                title = "Star Wars",
-                video = false,
-                voteAverage = 8.2
-            ),
-            topCast = listOf(
-                MovieDetailsScreenUiState.MovieTopCastUiState(
-                    castId = 1,
-                    castName = "Halle Bailey",
-                    castImage = "https://image.tmdb.org/t/p/w500/hLtxNK8eeWZkFSeaAASFWm15Qv0.jpg"
-                ),
-                MovieDetailsScreenUiState.MovieTopCastUiState(
-                    castId = 2,
-                    castName = "JonahHauer-King",
-                    castImage = "https://image.tmdb.org/t/p/w500/1GOW1cejmE8D8T6PRikYlGmUae0.jpg"
-                ),
-                MovieDetailsScreenUiState.MovieTopCastUiState(
-                    castId = 3,
-                    castName = "Daveed Diggs",
-                    castImage = "https://image.tmdb.org/t/p/w500/82rxrGxOqQW2NjKsIiNbDYHFfmb.jpg"
-                ),
-                MovieDetailsScreenUiState.MovieTopCastUiState(
-                    castId = 4,
-                    castName = "Awkwafina",
-                    castImage = "https://image.tmdb.org/t/p/w500/6mMczfjM8CiS1WuBOgo5Xom1TcR.jpg"
-                ),
-                MovieDetailsScreenUiState.MovieTopCastUiState(
-                    castId = 5,
-                    castName = "hLtxNK8ee",
-                    castImage = "https://image.tmdb.org/t/p/w500/1GOW1cejmE8D8T6PRikYlGmUae0.jpg"
-                ),
-            ),
-            images = MovieDetailsScreenUiState.MovieImagesUiState(
-                image = "test"
-            ),
-            keyWords = MovieDetailsScreenUiState.MovieKeyWordsUiState(
-                test = "test"
-            ),
-            recommendations = MovieDetailsScreenUiState.MovieRecommendationsUiState(
-                test = "test"
-            ),
-            reviews = MovieDetailsScreenUiState.MovieReviewsUiState(
-                test = "test"
-            ),
-            similar = MovieDetailsScreenUiState.MovieSimilarUiState(
-                test = "test"
-            ),
-        )
     }
 }
