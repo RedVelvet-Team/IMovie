ec76317965d54cb4a361f0a71baddf69b2dabb52
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 16:00:37 +0300

    add movie composables to content

--- ui/src/main/java/com/redvelvet/ui/composable/MediaInfoCard.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/MediaInfoCard.kt
@@ -1,12 +1,16 @@
 package com.redvelvet.ui.composable
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import com.redvelvet.ui.R
 import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.spacing
@@ -14,6 +18,7 @@ import com.redvelvet.ui.theme.spacing
 @Composable
 fun MediaInfoCard(
     data: MediaInfoCardData,
+    onGenreClick: (String) -> Unit,
 ) {
     Row {
         ItemBasicCard(
@@ -23,40 +28,66 @@ fun MediaInfoCard(
         )
         SpacerHorizontal(width = MaterialTheme.spacing.spacing12)
         Column {
-            Text(
-                text = data.originalTitle,
-                style = Typography.titleLarge,
-                color = Color.White
-            )
+            Box(
+                modifier = Modifier
+                    .padding(end = MaterialTheme.spacing.spacing16)
+            ) {
+                Text(
+                    text = data.originalTitle,
+                    style = Typography.titleLarge,
+                    color = Color.White,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
             SpacerVertical(height = MaterialTheme.spacing.spacing16)
             LazyRow {
                 items(data.genres.size) {
-                    GenreButton(genre = data.genres[it])
+                    GenreButton(genre = data.genres[it], onGenreClick = onGenreClick)
                 }
             }
             SpacerVertical(height = MaterialTheme.spacing.spacing12)
+            Box(
+                modifier = Modifier
+                    .padding(end = MaterialTheme.spacing.spacing16)
+            ) {}
             if (data.hasTime) {
-                LabeledValueHorizontal(
-                    movieTime = data.movieTime,
-                    icon = R.drawable.movie_time,
-                    iconDescription = R.string.media_date
-                )
+                Box(
+                    modifier = Modifier
+                        .padding(end = MaterialTheme.spacing.spacing16)
+                ) {
+                    LabeledValueHorizontal(
+                        movieTime = data.movieTime,
+                        icon = R.drawable.movie_time,
+                        iconDescription = R.string.media_date,
+                    )
+                }
                 SpacerVertical(height = MaterialTheme.spacing.spacing8)
             }
             if (data.hasDate) {
-                LabeledValueHorizontal(
-                    movieTime = data.seriesDate,
-                    icon = R.drawable.icon_date,
-                    iconDescription = R.string.media_time
-                )
+                Box(
+                    modifier = Modifier
+                        .padding(end = MaterialTheme.spacing.spacing16)
+                ) {
+                    LabeledValueHorizontal(
+                        movieTime = data.seriesDate,
+                        icon = R.drawable.icon_date,
+                        iconDescription = R.string.media_time
+                    )
+                }
                 SpacerVertical(height = MaterialTheme.spacing.spacing8)
             }
             SpacerVertical(height = MaterialTheme.spacing.spacing8)
-            LabeledValueHorizontal(
-                movieTime = data.spokenLanguages,
-                icon = R.drawable.global,
-                iconDescription = R.string.media_language
-            )
+            Box(
+                modifier = Modifier
+                    .padding(end = MaterialTheme.spacing.spacing16)
+            ) {
+                LabeledValueHorizontal(
+                    movieTime = data.spokenLanguages,
+                    icon = R.drawable.global,
+                    iconDescription = R.string.media_language
+                )
+            }
         }
 
     }
--- ui/src/main/java/com/redvelvet/ui/screen/movieDetails/mediaComposable/MediaDetailsForegroundContent.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/movieDetails/mediaComposable/MediaDetailsForegroundContent.kt
@@ -1,18 +1,26 @@
 package com.redvelvet.ui.screen.movieDetails.mediaComposable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import com.redvelvet.ui.composable.CircularImageAvatar
+import com.redvelvet.ui.composable.ItemsSection
 import com.redvelvet.ui.composable.LongExpandedText
 import com.redvelvet.ui.composable.MediaInfoCard
 import com.redvelvet.ui.composable.MediaInfoCardData
@@ -21,11 +29,11 @@ import com.redvelvet.ui.screen.movieDetails.MovieDetailsUiEvent
 import com.redvelvet.ui.composable.SpacerVertical
 import com.redvelvet.ui.screen.movieDetails.MovieDetailsScreen
 import com.redvelvet.ui.screen.movieDetails.MovieEventsTest
+import com.redvelvet.ui.theme.OnSecondary
 import com.redvelvet.ui.theme.Primary
 import com.redvelvet.ui.theme.radius
 import com.redvelvet.ui.theme.spacing
 import com.redvelvet.viewmodel.movieDetails.MovieDetailsScreenUiState
-
 
 @Preview(
     showSystemUi = true,
@@ -73,15 +81,53 @@ fun MediaDetailsForegroundContent(
                         hasDate = false,
                         it.runtime,
                         it.spokenLanguages,
-                    )
+                    ),
+                    onGenreClick = { uiEvent.onMovieCategory("") }
                 )
                 SpacerVertical(height = MaterialTheme.spacing.spacing24)
                 MediaRateRow(it.voteAverage.toString(), { uiEvent.onRateMedia(5.5) })
                 SpacerVertical(height = MaterialTheme.spacing.spacing24)
-                LongExpandedText(it.overview)
-                SpacerVertical(height = MaterialTheme.spacing.spacing24)
+                Box(
+                    modifier = Modifier
+                        .padding(end = MaterialTheme.spacing.spacing16)
+                ) {
+                    LongExpandedText(it.overview)
+                }
             }
+        }
+        SpacerVertical(height = MaterialTheme.spacing.spacing24)
+        state.data?.topCast?.let {
+            ItemsSection(
+                label = "Top Cast",
+                images = it.map { it2 -> it2.castImage },
+                hasName = true,
+                name = it.map { it2 -> it2.castName },
+                hasCustomList = true,
+                customListItemComposable = { index, image ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .clickable { uiEvent.onCast(it[index].castId) }
+                    ) {
+                        CircularImageAvatar(imageUrl = image)
+                        SpacerVertical(height = MaterialTheme.spacing.spacing4)
+                        Box(
+                            modifier = Modifier
+                                .width(70.dp)
+                        ) {
+                            Text(
+                                text = it[index].castName,
+                                style = MaterialTheme.typography.bodySmall,
+                                color = OnSecondary,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        }
+                    }
+                },
+            )
         }
     }
 
 }
+
