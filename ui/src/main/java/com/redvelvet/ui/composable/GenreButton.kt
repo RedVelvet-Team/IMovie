ed42b8ddfaac9fe33fce06a53cdec6ea6e49eaff
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 15:56:24 +0300

    correct genre buttons

--- ui/src/main/java/com/redvelvet/ui/composable/GenreButton.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/GenreButton.kt
@@ -1,16 +1,18 @@
 package com.redvelvet.ui.composable
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
-import com.redvelvet.ui.theme.FontSecondary
+import com.redvelvet.ui.theme.FontPrimary
 import com.redvelvet.ui.theme.OnSecondary
 import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.dimens
@@ -18,24 +20,30 @@ import com.redvelvet.ui.theme.radius
 import com.redvelvet.ui.theme.spacing
 
 @Composable
-fun GenreButton(genre: String) {
-    Button(
+fun GenreButton(genre: String, onGenreClick: (String) -> Unit) {
+    Box(
         modifier = Modifier
-            .height(MaterialTheme.dimens.dimens24)
-            .padding(end = MaterialTheme.spacing.spacing4),
-        onClick = {},
-        colors = ButtonDefaults.buttonColors(OnSecondary),
-        shape = RoundedCornerShape(MaterialTheme.radius.radius8)
+            .padding(end = MaterialTheme.spacing.spacing8),
     ) {
-        Text(
-            text = genre,
-            textAlign = TextAlign.Center,
+        Box(
             modifier = Modifier
-                .padding(
-                    horizontal = MaterialTheme.spacing.spacing8,
-                    vertical = MaterialTheme.spacing.spacing4
-                ),
-            style = Typography.titleSmall.copy(color = FontSecondary)
-        )
+                .clip(RoundedCornerShape(MaterialTheme.radius.radius8))
+                .height(MaterialTheme.dimens.dimens24)
+                .clickable { onGenreClick(genre) }
+                .background(OnSecondary),
+        ) {
+            Text(
+                text = genre,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(
+                        horizontal = MaterialTheme.spacing.spacing8,
+                        vertical = MaterialTheme.spacing.spacing4
+                    ),
+                style = Typography.labelSmall.copy(
+                    color = FontPrimary,
+                )
+            )
+        }
     }
 }
