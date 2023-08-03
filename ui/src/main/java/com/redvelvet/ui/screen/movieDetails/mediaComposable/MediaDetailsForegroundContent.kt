50e1957b1ff7f5736ebdc59771ce78a82920ed6a
Taha Saad <flutterdev.taha.saad@gmail.com>
2023-08-03 05:56:11 +0300

    add LongExpandedText composable and add it to content

--- ui/src/main/java/com/redvelvet/ui/screen/movieDetails/mediaComposable/MediaDetailsForegroundContent.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/movieDetails/mediaComposable/MediaDetailsForegroundContent.kt
@@ -1,46 +1,42 @@
 package com.redvelvet.ui.screen.movieDetails.mediaComposable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.rememberNavController
-import com.redvelvet.ui.R
-import com.redvelvet.ui.composable.CustomButton
-import com.redvelvet.ui.composable.GenreButton
+import com.redvelvet.ui.composable.LongExpandedText
+import com.redvelvet.ui.composable.MediaRateRow
 import com.redvelvet.ui.screen.movieDetails.MovieDetailsUiEvent
-import com.redvelvet.ui.composable.ItemBasicCard
-import com.redvelvet.ui.composable.LabeledValueHorizontal
-import com.redvelvet.ui.composable.SpacerHorizontal
 import com.redvelvet.ui.composable.SpacerVertical
 import com.redvelvet.ui.screen.movieDetails.MovieDetailsScreen
 import com.redvelvet.ui.screen.movieDetails.MovieEventsTest
 import com.redvelvet.ui.theme.FontPrimary
-import com.redvelvet.ui.theme.FontSecondary
-import com.redvelvet.ui.theme.OnSecondary
 import com.redvelvet.ui.theme.Primary
-import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.radius
 import com.redvelvet.ui.theme.spacing
 import com.redvelvet.viewmodel.movieDetails.MovieDetailsScreenUiState
@@ -84,46 +80,13 @@ fun MediaDetailsForegroundContent(
                     .padding(start = MaterialTheme.spacing.spacing16)
             ) {
                 MediaInfoCard(it)
+                SpacerVertical(height = MaterialTheme.spacing.spacing24)
+                MediaRateRow(it.voteAverage.toString(), { uiEvent.onRateMedia(5.5) })
+                SpacerVertical(height = MaterialTheme.spacing.spacing24)
+                LongExpandedText(it.overview)
+                SpacerVertical(height = MaterialTheme.spacing.spacing24)
+
             }
         }
-    }
-}
-
-@Composable
-fun MediaInfoCard(movieDetails: MovieDetailsScreenUiState.MovieDetailsUiState) {
-    Row {
-        ItemBasicCard(
-            image = movieDetails.posterPath,
-            hasName = false,
-            name = movieDetails.originalTitle,
-            hasDateAndCountry = false,
-        )
-        SpacerHorizontal(width = MaterialTheme.spacing.spacing12)
-        Column {
-            Text(
-                text = movieDetails.originalTitle,
-                style = Typography.headlineMedium,
-                color = Color.White
-            )
-            SpacerVertical(height = MaterialTheme.spacing.spacing16)
-            LazyRow {
-                items(movieDetails.genres.size) {
-                    GenreButton(genre = movieDetails.genres[it])
-                }
-            }
-            SpacerVertical(height = MaterialTheme.spacing.spacing12)
-            LabeledValueHorizontal(
-                movieTime = movieDetails.runtime,
-                icon = R.drawable.movie_time,
-                iconDescription = R.string.movie_time
-            )
-            SpacerVertical(height = MaterialTheme.spacing.spacing8)
-            LabeledValueHorizontal(
-                movieTime = movieDetails.releaseDate,
-                icon = R.drawable.global,
-                iconDescription = R.string.movie_language
-            )
-        }
-
     }
 }
