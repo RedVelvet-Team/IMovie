7bc532e1a3bc93e527e3c152e42cfe83b2a625ed
haidy <haidyabugom3a@gmail.com>
2023-08-05 02:04:59 +0300

    add new values to dimens

--- ui/src/main/java/com/redvelvet/ui/theme/Dimens.kt
+++ ui/src/main/java/com/redvelvet/ui/theme/Dimens.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.unit.dp
 data class Dimens(
     val dimens1: Dp = 1.dp,
     val dimens8: Dp = 8.dp,
+    val dimens4: Dp = 8.dp,
     val dimens16: Dp = 16.dp,
     val dimens24: Dp = 24.dp,
     val dimens32: Dp = 32.dp,
