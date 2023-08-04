7fe38dcc57ee29b7f677001b641c93d2e7fa18b3
Bassam <bassamouf@std.mans.edu.eg>
2023-08-05 02:26:19 +0300

    refactor system design onBoardingScreen

--- ui/src/main/java/com/redvelvet/ui/screen/onboarding/OnBoardingScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/onboarding/OnBoardingScreen.kt
@@ -5,8 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -16,14 +19,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.redvelvet.ui.R
-import com.redvelvet.ui.composable.PrimaryButton
 import com.redvelvet.ui.composable.CustomText
+import com.redvelvet.ui.composable.PrimaryButton
 import com.redvelvet.ui.composable.WallPaper
 import com.redvelvet.ui.navigation.MovieDestination
 import com.redvelvet.ui.screen.login.navigateToLogin
@@ -31,6 +33,8 @@ import com.redvelvet.ui.theme.FontPrimary
 import com.redvelvet.ui.theme.FontSecondary
 import com.redvelvet.ui.theme.Primary
 import com.redvelvet.ui.theme.Typography
+import com.redvelvet.ui.theme.dimens
+import com.redvelvet.ui.theme.spacing
 import com.redvelvet.viewmodel.onboarding.OnBoardingUiEvent
 import com.redvelvet.viewmodel.onboarding.OnBoardingViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -77,18 +81,21 @@ private fun OnBoardingContent(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = 64.dp),
+                .padding(horizontal = MaterialTheme.spacing.spacing64),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
             Image(
                 painter = painterResource(id = R.drawable.vector_logo),
                 contentDescription = stringResource(R.string.logo),
-                modifier = Modifier.size(height = 78.dp, width = 78.dp),
+                modifier = Modifier.size(
+                    height = MaterialTheme.dimens.dimens78,
+                    width = MaterialTheme.dimens.dimens78
+                ),
             )
             CustomText(
                 name = stringResource(R.string.name_app),
-                modifier = Modifier.padding(vertical = 16.dp),
+                modifier = Modifier.padding(vertical = MaterialTheme.spacing.spacing16),
                 style = Typography.headlineLarge.copy(color = FontPrimary),
             )
             CustomText(
@@ -97,9 +104,12 @@ private fun OnBoardingContent(
             )
         }
         Column(
-            modifier = Modifier.padding(bottom = 64.dp)
+            modifier = Modifier.padding(bottom = MaterialTheme.spacing.spacing64)
         ) {
             PrimaryButton(
+                modifier = Modifier
+                    .height(MaterialTheme.dimens.dimens49)
+                    .width(MaterialTheme.dimens.dimens100),
                 text = stringResource(R.string.onboarding_start),
                 onClick = { onStartClick() })
         }
