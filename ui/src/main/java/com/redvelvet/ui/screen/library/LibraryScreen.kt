aafe3339acae905acbd94f59c2a68fb2ac2b83d9
haidy <haidyabugom3a@gmail.com>
2023-08-05 13:34:45 +0300

    refactor library screen

--- ui/src/main/java/com/redvelvet/ui/screen/library/LibraryScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/library/LibraryScreen.kt
@@ -7,21 +7,23 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.style.TextAlign
 import com.redvelvet.ui.R
-import com.redvelvet.ui.composable.PrimaryButton
-import com.redvelvet.ui.composable.CustomText
 import com.redvelvet.ui.composable.FilxTopAppBar
+import com.redvelvet.ui.composable.PrimaryButton
+import com.redvelvet.ui.theme.BackgroundPrimary
 import com.redvelvet.ui.theme.FontPrimary
 import com.redvelvet.ui.theme.FontSecondary
-import com.redvelvet.ui.theme.Primary
 import com.redvelvet.ui.theme.Typography
+import com.redvelvet.ui.theme.spacing
 
 @Composable
 fun LibraryScreen() {
@@ -35,7 +37,7 @@ fun LibraryScreen() {
                 modifier = Modifier.fillMaxWidth()
             )
         },
-        containerColor = Primary,
+        containerColor = BackgroundPrimary,
     ) { paddingValues ->
         LibraryContent(paddingValues = paddingValues)
     }
@@ -46,7 +48,7 @@ private fun LibraryContent(paddingValues: PaddingValues) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 58.dp),
+            .padding(horizontal = MaterialTheme.spacing.spacing58),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -54,18 +56,21 @@ private fun LibraryContent(paddingValues: PaddingValues) {
             painter = painterResource(id = R.drawable.library_logo),
             contentDescription = stringResource(R.string.library_logo)
         )
-        CustomText(
-            name = stringResource(R.string.login_require_header),
-            modifier = Modifier.padding(top = 32.dp),
+        Text(
+            text = stringResource(R.string.login_require_header),
             style = Typography.headlineLarge.copy(color = FontPrimary),
+            modifier = Modifier.padding(top = MaterialTheme.spacing.spacing32),
+            textAlign = TextAlign.Center,
         )
-        CustomText(
-            name = stringResource(R.string.login_require_description),
-            modifier = Modifier.padding(top = 4.dp),
+        Text(
+            text = stringResource(R.string.login_require_description),
             style = Typography.titleSmall.copy(color = FontSecondary),
+            modifier = Modifier.padding(top = MaterialTheme.spacing.spacing4),
+            textAlign = TextAlign.Center,
         )
+
         Column(
-            modifier = Modifier.padding(top = 32.dp)
+            modifier = Modifier.padding(top = MaterialTheme.spacing.spacing32)
         ) {
             PrimaryButton(
                 text = stringResource(R.string.login_require_button),
