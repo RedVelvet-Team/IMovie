7b2ffb7e36ed6af619eb9d86024c3238a058b276
haidy <haidyabugom3a@gmail.com>
2023-08-05 13:51:24 +0300

    refactor items section

--- ui/src/main/java/com/redvelvet/ui/composable/ItemsSection.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/ItemsSection.kt
@@ -2,10 +2,16 @@ package com.redvelvet.ui.composable
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.rememberAsyncImagePainter
+import com.redvelvet.ui.theme.dimens
 import com.redvelvet.ui.theme.spacing
 
 @Composable
@@ -19,14 +25,17 @@ fun ItemsSection(
     country: List<String> = emptyList()
 ) {
     SectionHeader(label)
-    VerticalSpacer(space = 8)
     LazyRow(
+        modifier = Modifier.padding(top = MaterialTheme.spacing.spacing8),
         horizontalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.spacing8),
         contentPadding = PaddingValues(horizontal = MaterialTheme.spacing.spacing16)
     ) {
         itemsIndexed(images) { index, image ->
             ItemBasicCard(
-                image = image,
+                imagePainter = rememberAsyncImagePainter(model = image),
+                modifier = Modifier
+                    .width(MaterialTheme.dimens.dimens104)
+                    .height(MaterialTheme.dimens.dimens130),
                 hasName = hasName,
                 name = name[index],
                 hasDateAndCountry = hasDateAndCountry,
