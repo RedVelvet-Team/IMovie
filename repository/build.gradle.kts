d0c00d6cb93f6d648252dbf8f489266db8ccee81
abaferas <wasfyelgamed@gmail.com>
2023-08-06 18:39:16 +0300

    centralize gradle files all into one source

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -39,16 +39,15 @@ android {
 
 dependencies {
     implementation(project(":usecase"))
-    implementation("androidx.core:core-ktx:1.10.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(Deps.coreKtx)
+    testImplementation(Deps.junit)
+    androidTestImplementation(Deps.junitExt)
     //hilt
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-compiler:2.46.1")
-    implementation("androidx.hilt:hilt-work:1.0.0")
+    implementation(Deps.hiltAndroid)
+    kapt(Deps.hiltCompiler)
+    implementation(Deps.hiltWork)
     //gson
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation(Deps.gson)
     //retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation(Deps.retrofit)
 }
