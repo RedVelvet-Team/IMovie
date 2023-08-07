40ac7f9f880295fa4b1e7e1a00a8c0a5e411fcd9
haidy <haidyabugom3a@gmail.com>
2023-08-07 03:17:16 +0300

    fix tab layout

--- ui/src/main/java/com/redvelvet/ui/composable/FilxTabLayout.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/FilxTabLayout.kt
@@ -6,42 +6,44 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
-import androidx.compose.material3.TabRowDefaults
-import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.color
 import com.redvelvet.ui.theme.dimens
 import com.redvelvet.ui.theme.spacing
-
+import com.redvelvet.viewmodel.home.HomeViewModel
 
 @Composable
-fun HomeTabLayout() {
-    var state by remember { mutableStateOf(0) }
-    val titles = listOf("Movies", "TV Shows")
+fun FilxTabLayout(viewModel: HomeViewModel = HomeViewModel()) {
+    val state by viewModel.state.collectAsState()
+    FilxTabLayoutContent(state.selectedTabIndex, state.tabLayoutTitles, viewModel::onClickTab)
+}
+
+@Composable
+fun FilxTabLayoutContent(
+    selectedTabIndex: Int,
+    titles: List<String>,
+    onClickTab: (Int) -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(horizontal = MaterialTheme.spacing.spacing16)
     ) {
         TabRow(
-            selectedTabIndex = state,
+            selectedTabIndex = selectedTabIndex,
             containerColor = MaterialTheme.color.backgroundPrimary,
             indicator = {
             },
@@ -50,8 +52,8 @@ fun HomeTabLayout() {
             titles.forEachIndexed { index, title ->
                 Box {
                     Tab(
-                        selected = state == index,
-                        onClick = { state = index },
+                        selected = selectedTabIndex == index,
+                        onClick = { onClickTab(index) },
                         text = {
                             Text(
                                 text = title,
@@ -61,7 +63,7 @@ fun HomeTabLayout() {
                                 style = Typography.headlineSmall
                             )
                         })
-                    if (state == index) {
+                    if (selectedTabIndex == index) {
                         Box(
                             Modifier
                                 .fillMaxWidth(0.6f)
@@ -78,6 +80,6 @@ fun HomeTabLayout() {
 
 @Preview
 @Composable
-fun previewTabLayout(){
-    HomeTabLayout()
+fun previewTabLayout() {
+    FilxTabLayout()
 }
