8f13cd0910087ca52503d2720c7177d968d5bc16
HassanAyman <hassan3ayman@gmail.com>
2023-08-14 21:15:23 +0300

    make teh status bar transparent

--- ui/src/main/java/com/redvelvet/ui/main/MainActivity.kt
+++ ui/src/main/java/com/redvelvet/ui/main/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.redvelvet.ui.MovieApp
 import com.redvelvet.ui.theme.IMovieTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,9 +18,16 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             IMovieTheme {
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    // set transparent color so that our image is visible
+                    // behind the status bar
+                    systemUiController.setStatusBarColor(color = Color.Transparent)
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
