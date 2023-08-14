a70266f84c8a6a68f11bda7b07fc2ccc0f107ff6
abaferas <wasfyelgamed@gmail.com>
2023-08-15 02:31:31 +0300

    edit start destination to splash screen

--- ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
+++ ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
@@ -1,22 +1,21 @@
 package com.redvelvet.ui.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import com.redvelvet.ui.LocalNavController
+import com.redvelvet.ui.screen.forgot_password.forgotPasswordRoute
 import com.redvelvet.ui.screen.home.homeRoute
 import com.redvelvet.ui.screen.login.loginRoute
+import com.redvelvet.ui.screen.movieDetails.movieDetailsRoute
 import com.redvelvet.ui.screen.onboarding.onBoardingRoute
 import com.redvelvet.ui.screen.search.searchRoute
 import com.redvelvet.ui.screen.seeAllUpcoming.seeAllUpcomingListRoute
-import com.redvelvet.ui.screen.forgot_password.forgotPasswordRoute
-import com.redvelvet.ui.screen.movieDetails.movieDetailsRoute
 import com.redvelvet.ui.screen.signup.signupRoute
 import com.redvelvet.ui.screen.splash.splashRoute
 
 @Composable
 fun MovieNavGraph() {
-    NavHost(LocalNavController.current, startDestination = MovieDestination.MovieDetails.route) {
+    NavHost(LocalNavController.current, startDestination = MovieDestination.Splash.route) {
         splashRoute()
         onBoardingRoute()
         searchRoute()
