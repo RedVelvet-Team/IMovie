330838816c818bfacbf0ff4cebde00670951018b
HassanAyman <hassan3ayman@gmail.com>
2023-08-14 22:50:27 +0300

    implement error entitiy in the remote data source and fix exceptions

--- repository/src/main/java/com/redvelvet/repository/repository/BaseRepository.kt
+++ repository/src/main/java/com/redvelvet/repository/repository/BaseRepository.kt
@@ -1,30 +1,7 @@
 package com.redvelvet.repository.repository
 
-import com.redvelvet.entities.error.MovieError
-import com.redvelvet.entities.error.NetworkError
-import com.redvelvet.entities.error.NullResultError
-import com.redvelvet.entities.error.ServerError
-import com.redvelvet.entities.error.ValidationError
-import com.redvelvet.repository.util.NoInternetException
-import com.redvelvet.repository.util.NullResultException
-import com.redvelvet.repository.util.RemoteException
-import com.redvelvet.repository.util.ServerException
-import com.redvelvet.repository.util.ValidationException
-
 open class BaseRepository {
     protected suspend fun <T> wrapRemoteResponse(response: suspend () -> T): T {
-        return try {
-            response()
-        } catch (e: NoInternetException) {
-            throw NetworkError()
-        }catch (e: NullResultException){
-            throw NullResultError()
-        }catch (e: ServerException){
-            throw ServerError()
-        }catch (e: ValidationException){
-            throw ValidationError()
-        }catch (e: RemoteException){
-            throw MovieError()
-        }
+        return response()
     }
 }
