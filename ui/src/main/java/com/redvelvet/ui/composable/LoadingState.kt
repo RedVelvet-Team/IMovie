7518776edea4aab4f6c9b1c0091cf9b5a47da002
abaferas <wasfyelgamed@gmail.com>
2023-08-15 02:30:49 +0300

    refactor LoadingState and add ErrorState also

--- ui/src/main/java/com/redvelvet/ui/composable/LoadingState.kt
+++ ui/src/main/java/com/redvelvet/ui/composable/LoadingState.kt
@@ -2,10 +2,14 @@ package com.redvelvet.ui.composable
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.redvelvet.ui.R
 
 @Composable
 fun LoadingState() {
@@ -13,6 +17,18 @@ fun LoadingState() {
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        CircularProgressIndicator()
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.loading))
+        LottieAnimation(composition = composition, iterations = 50)
+    }
+}
+
+@Composable
+fun ErrorState() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.error))
+        LottieAnimation(composition = composition, iterations = 50)
     }
 }
