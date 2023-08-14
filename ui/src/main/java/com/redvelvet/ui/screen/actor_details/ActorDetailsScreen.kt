6d2204f1687e4bc8b5f9415af2101807a86c03a1
HassanAyman <hassan3ayman@gmail.com>
2023-08-15 02:15:35 +0300

    implement viewmodel request and state and screen for actor details

--- ui/src/main/java/com/redvelvet/ui/screen/actor_details/ActorDetailsScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/actor_details/ActorDetailsScreen.kt
@@ -1,9 +1,5 @@
 package com.redvelvet.ui.screen.actor_details
 
-import android.app.Activity
-import android.app.StatusBarManager
-import android.view.WindowInsets.Side
-import android.view.WindowManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -12,48 +8,36 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.MyLocation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsControllerCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.compose.rememberAsyncImagePainter
-import com.airbnb.lottie.parser.FloatParser
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.redvelvet.ui.R
 import com.redvelvet.ui.composable.ItemBasicCard
-import com.redvelvet.ui.theme.BackgroundOnPrimary
 import com.redvelvet.ui.theme.BackgroundPrimary
 import com.redvelvet.ui.theme.Shadow1
 import com.redvelvet.ui.theme.Shadow2
@@ -62,6 +46,7 @@ import com.redvelvet.ui.theme.Shadow4
 import com.redvelvet.ui.theme.color
 import com.redvelvet.ui.theme.radius
 import com.redvelvet.ui.theme.spacing
+import com.redvelvet.viewmodel.actor_details.ActorDetailsUiState
 import com.redvelvet.viewmodel.actor_details.ActorDetailsViewModel
 
 @Composable
@@ -69,16 +54,18 @@ import com.redvelvet.viewmodel.actor_details.ActorDetailsViewModel
 fun ActorDetailsScreen(
     viewModel: ActorDetailsViewModel = hiltViewModel()
 ) {
-    ActorDetailsContent()
+    val state by viewModel.state.collectAsState()
+    ActorDetailsContent(state)
 }
 
 @Composable
-private fun ActorDetailsContent() {
+private fun ActorDetailsContent(state: ActorDetailsUiState) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(BackgroundPrimary)
             .verticalScroll(state = rememberScrollState())
+            .padding(bottom = MaterialTheme.spacing.spacing24)
     ) {
         Box(
             modifier = Modifier
@@ -89,7 +76,7 @@ private fun ActorDetailsContent() {
                 modifier = Modifier
                     .fillMaxSize(),
                 painter = painterResource(id = R.drawable.image),
-                contentDescription = "",
+                contentDescription = state.name,
                 contentScale = ContentScale.Crop
             )
             Spacer(
@@ -109,7 +96,7 @@ private fun ActorDetailsContent() {
             )
             Text(
                 modifier = Modifier.align(Alignment.BottomCenter),
-                text = "Freddie Highmore",
+                text = state.name,
                 style = MaterialTheme.typography.headlineLarge,
                 color = MaterialTheme.color.fontPrimary,
                 textAlign = TextAlign.Center
@@ -117,12 +104,14 @@ private fun ActorDetailsContent() {
         }
         Text(
             modifier = Modifier.fillMaxWidth(),
-            text = "Actor",
+            text = state.knownForDepartment,
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.color.fontPrimary,
             textAlign = TextAlign.Center
         )
-        DisplayCard(
+        BirthCard(
+            birthDate = state.birthDate,
+            birthLocation = state.birthLocation,
             modifier = Modifier.padding(
                 top = MaterialTheme.spacing.spacing32,
                 start = MaterialTheme.spacing.spacing16,
@@ -130,6 +119,9 @@ private fun ActorDetailsContent() {
             )
         )
         DisplayCard(
+            iconPainter = painterResource(id = R.drawable.icon_game),
+            title = stringResource(R.string.known_as),
+            text = state.knownAs,
             modifier = Modifier.padding(
                 top = MaterialTheme.spacing.spacing16,
                 start = MaterialTheme.spacing.spacing16,
@@ -138,7 +130,7 @@ private fun ActorDetailsContent() {
         )
         Text(
             modifier = Modifier.padding(top = MaterialTheme.spacing.spacing24, start = MaterialTheme.spacing.spacing16),
-            text = "Known For",
+            text = stringResource(R.string.known_for),
             style = MaterialTheme.typography.headlineMedium,
             color = MaterialTheme.color.fontPrimary,
             textAlign = TextAlign.Center
@@ -162,15 +154,14 @@ private fun ActorDetailsContent() {
         }
         Text(
             modifier = Modifier.padding(top = MaterialTheme.spacing.spacing24, start = MaterialTheme.spacing.spacing16),
-            text = "Bigoraphy",
+            text = stringResource(R.string.biography),
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.color.fontPrimary,
             textAlign = TextAlign.Center
         )
         Text(
             modifier = Modifier.padding(top = MaterialTheme.spacing.spacing8, start = MaterialTheme.spacing.spacing16),
-            text = "Halle Lynn Bailey (born March 27, 2000)\n" +
-                    "is an American singer-songwriter and actress. She became known for being one half of the musical duo Chloe x Halle with her sister ChloeBailey. They have released ...Read more",
+            text = state.biography,
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.color.fontSecondary,
         )
@@ -178,8 +169,35 @@ private fun ActorDetailsContent() {
 }
 
 @Composable
+fun BirthCard(
+    birthDate: String,
+    birthLocation: String,
+    modifier: Modifier = Modifier,
+) {
+    DisplayCard(
+        iconPainter = painterResource(id = R.drawable.icon_game),
+        title = stringResource(R.string.born),
+        text = birthDate,
+        modifier = modifier
+    ) {
+        Text(
+            modifier = Modifier.padding(top = MaterialTheme.spacing.spacing4),
+            text = birthLocation,
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.color.fontSecondary,
+            textAlign = TextAlign.Center
+        )
+
+    }
+}
+
+@Composable
 fun DisplayCard(
-    modifier: Modifier = Modifier
+    iconPainter: Painter,
+    title: String,
+    text: String,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit = {}
 ) {
     Column(
         modifier = modifier
@@ -194,12 +212,12 @@ fun DisplayCard(
             Icon(
                 modifier = Modifier
                     .size(MaterialTheme.spacing.spacing20),
-                imageVector = Icons.Outlined.MyLocation,
+                painter = iconPainter,
                 contentDescription = ""
             )
             Text(
                 modifier = Modifier.padding(start = MaterialTheme.spacing.spacing8),
-                text = "Born",
+                text = title,
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.color.fontSecondary,
                 textAlign = TextAlign.Center
@@ -209,18 +227,11 @@ fun DisplayCard(
 
         Text(
             modifier = Modifier.padding(top = MaterialTheme.spacing.spacing8),
-            text = "27 march , 2000(23 years old",
+            text = text,
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.color.fontSecondary,
             textAlign = TextAlign.Center
         )
-        Text(
-            modifier = Modifier.padding(top = MaterialTheme.spacing.spacing4),
-            text = "Atlanta, Georgia",
-            style = MaterialTheme.typography.headlineSmall,
-            color = MaterialTheme.color.fontSecondary,
-            textAlign = TextAlign.Center
-        )
-
+        content()
     }
 }
--- viewmodels/src/main/java/com/redvelvet/viewmodel/actor_details/ActorDetailsViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/actor_details/ActorDetailsViewModel.kt
@@ -1,12 +1,46 @@
 package com.redvelvet.viewmodel.actor_details
 
+import com.redvelvet.entities.actor.Actor
+import com.redvelvet.usecase.usecase.GetActorDetailsUseCase
 import com.redvelvet.viewmodel.base.BaseViewModel
+import com.redvelvet.viewmodel.base.ErrorUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class ActorDetailsViewModel @Inject constructor(
-
+    private val getActorDetails: GetActorDetailsUseCase
 ): BaseViewModel<ActorDetailsUiState, ActorDetailsUiEffect>(ActorDetailsUiState()) {
-
+    
+    private val args = 520
+    init {
+        getDetails()
+    }
+    
+    private fun getDetails(){
+        tryToExecute(
+            execute = {getActorDetails(args)},
+            onSuccessWithData = ::onGetSuccess,
+            onError = ::onError
+        )
+    }
+    
+    private fun onGetSuccess(result: Actor){
+        _state.update { it.copy(
+            isLoading = false,
+            id = result.id,
+            name = result.name,
+            knownForDepartment = result.knownForDepartment,
+            birthDate = result.birthday,
+            birthLocation = result.placeOfBirth,
+            imageUrl = result.profileImageUrl,
+            knownAs = result.alsoKnownAs,
+            biography = result.biography
+        ) }
+    }
+    
+    private fun onError(error: ErrorUiState){
+        _state.update { it.copy(isLoading = false, error = error) }
+    }
 }
