321fd6d183a08a6c50d10d3e645d47c354de5351
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-14 23:39:04 +0300

    implement all tv shows ui state & view model and design

---
+++ ui/src/main/java/com/redvelvet/ui/screen/seealltv/SeeAllTvShowsRoute.kt
@@ -0,0 +1,17 @@
+package com.redvelvet.ui.screen.seealltv
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptionsBuilder
+import androidx.navigation.compose.composable
+import com.redvelvet.ui.navigation.MovieDestination
+
+fun NavGraphBuilder.seeAllTvListRoute() {
+    composable(route = MovieDestination.SeeAllTv.route) {
+        SeeAllTvScreen()
+    }
+}
+
+fun NavController.navigateSeeAllMovie(builder: NavOptionsBuilder.() -> Unit = {}) {
+    navigate(MovieDestination.SeeAllTv.route, builder = builder)
+}
