f719b4a5cd8a8c366aacef78685edc99ce8cbdd9
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-14 23:28:06 +0300

    add paging source to all tv shows functions

---
+++ repository/src/main/java/com/redvelvet/repository/pagingSource/seealltv/SeeAllAiringTodayTvPageSource.kt
@@ -0,0 +1,14 @@
+package com.redvelvet.repository.pagingSource.seealltv
+
+import com.redvelvet.repository.dto.tvShow.TvShowDto
+import com.redvelvet.repository.pagingSource.BasePagingSource
+import com.redvelvet.repository.source.RemoteDataSource
+import javax.inject.Inject
+
+class SeeAllAiringTodayTvPageSource @Inject constructor(
+    remoteDataSource: RemoteDataSource
+) : BasePagingSource<TvShowDto>(remoteDataSource) {
+    override suspend fun fetchData(page: Int): List<TvShowDto> {
+        return remoteDataSource.seeAllAiringTodayTv(page)
+    }
+}
