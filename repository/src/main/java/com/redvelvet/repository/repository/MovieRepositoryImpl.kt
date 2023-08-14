9b2e53452b26d9d6479c86d69aba672c9ad1fd1a
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-14 23:30:40 +0300

    add suspend tv shows functions in movie repository then implement them

--- repository/src/main/java/com/redvelvet/repository/repository/MovieRepositoryImpl.kt
+++ repository/src/main/java/com/redvelvet/repository/repository/MovieRepositoryImpl.kt
@@ -4,18 +4,25 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.PagingSource
+import androidx.paging.map
 import com.redvelvet.entities.actor.Actor
 import com.redvelvet.entities.movie.Movie
 import com.redvelvet.entities.search.SearchResult
 import com.redvelvet.entities.tv.TvShow
+import com.redvelvet.repository.dto.tvShow.TvShowDto
+import com.redvelvet.repository.mapper.toTvShow
 import com.redvelvet.repository.pagingSource.ActorSearchPageSource
 import com.redvelvet.repository.pagingSource.MoviesSearchPageSource
 import com.redvelvet.repository.pagingSource.MultiSearchPageSource
 import com.redvelvet.repository.pagingSource.TvShowSearchPageSource
+import com.redvelvet.repository.pagingSource.seealltv.SeeAllAiringTodayTvPageSource
+import com.redvelvet.repository.pagingSource.seealltv.SeeAllOnTheAirTvPageSource
+import com.redvelvet.repository.pagingSource.seealltv.SeeAllPopularTvPageSource
 import com.redvelvet.repository.source.LocalDataSource
 import com.redvelvet.repository.source.RemoteDataSource
 import com.redvelvet.usecase.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MovieRepositoryImpl @Inject constructor(
@@ -52,6 +59,48 @@ class MovieRepositoryImpl @Inject constructor(
     }
 
     // endregion
+
+    //region see all tv
+    override suspend fun seeAllAiringTodayTv(page: Int?): Flow<PagingData<TvShow>> {
+        return seeAll(
+            page = page,
+            mapper = TvShowDto::toTvShow,
+            sourceFactory = ::SeeAllAiringTodayTvPageSource
+        )
+    }
+
+    override suspend fun seeAllOnTheAir(page: Int?): Flow<PagingData<TvShow>> {
+        return seeAll(
+            page = page,
+            mapper = TvShowDto::toTvShow,
+            sourceFactory = ::SeeAllOnTheAirTvPageSource
+        )
+    }
+
+    override suspend fun seeAllPopularTv(page: Int?): Flow<PagingData<TvShow>> {
+        return seeAll(
+            page = page,
+            mapper = TvShowDto::toTvShow,
+            sourceFactory = ::SeeAllPopularTvPageSource
+        )
+    }
+    //endregion
+
+    //region wrapper
+    private fun <I : Any, O : Any> seeAll(
+        page: Int?,
+        sourceFactory: (RemoteDataSource) -> PagingSource<Int, I>,
+        mapper: I.() -> O
+    ): Flow<PagingData<O>> {
+        return Pager(
+            config = PagingConfig(pageSize = page ?: DEFAULT_PAGE_SIZE),
+            pagingSourceFactory = { sourceFactory(remoteDataSource) }
+        ).flow.map { pagingData ->
+            pagingData.map { it.mapper() }
+        }
+    }
+    //endregion
+
     companion object {
         private const val DEFAULT_PAGE_SIZE = 10
     }
--- usecase/src/main/java/com/redvelvet/usecase/repository/MovieRepository.kt
+++ usecase/src/main/java/com/redvelvet/usecase/repository/MovieRepository.kt
@@ -9,8 +9,15 @@ import kotlinx.coroutines.flow.Flow
 
 
 interface MovieRepository {
-    fun multiSearch(query: String, page: Int?=1): Flow<PagingData<SearchResult>>
-    fun searchPeople(query: String, page: Int?=1): Flow<PagingData<Actor>>
-    fun searchMovie(query: String, page: Int?=1): Flow<PagingData<Movie>>
-    fun searchTvShows(query: String, page: Int?=1): Flow<PagingData<TvShow>>
+    fun multiSearch(query: String, page: Int? = 1): Flow<PagingData<SearchResult>>
+    fun searchPeople(query: String, page: Int? = 1): Flow<PagingData<Actor>>
+    fun searchMovie(query: String, page: Int? = 1): Flow<PagingData<Movie>>
+    fun searchTvShows(query: String, page: Int? = 1): Flow<PagingData<TvShow>>
+
+    //region see all
+    suspend fun seeAllAiringTodayTv(page: Int?): Flow<PagingData<TvShow>>
+    suspend fun seeAllOnTheAir(page: Int?): Flow<PagingData<TvShow>>
+    suspend fun seeAllPopularTv(page: Int?): Flow<PagingData<TvShow>>
+
+    //endregion
 }
