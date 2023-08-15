1991a093f25428caf6df413ef3915d2955d4d9a8
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-08-16 02:50:06 +0300

    make splash screen startDestination

--- ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
+++ ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
@@ -16,7 +16,7 @@ import com.redvelvet.ui.screen.splash.splashRoute
 
 @Composable
 fun MovieNavGraph() {
-    NavHost(LocalNavController.current, startDestination = MovieDestination.SeeAllTvShow.route) {
+    NavHost(LocalNavController.current, startDestination = MovieDestination.Splash.route) {
         splashRoute()
         onBoardingRoute()
         searchRoute()
