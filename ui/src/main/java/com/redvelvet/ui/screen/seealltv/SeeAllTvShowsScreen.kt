8607a43813dddd84c2275ac840075610eac2dcc1
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-15 22:53:20 +0300

    fix image card and make it appear

--- ui/src/main/java/com/redvelvet/ui/screen/seealltv/SeeAllTvShowsScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/seealltv/SeeAllTvShowsScreen.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
@@ -22,6 +24,7 @@ import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.redvelvet.ui.composable.ItemBasicCard
 import com.redvelvet.ui.composable.MovieScaffold
 import com.redvelvet.ui.theme.color
+import com.redvelvet.ui.theme.dimens
 import com.redvelvet.ui.theme.spacing
 import com.redvelvet.viewmodel.home.TvShowUiState
 import com.redvelvet.viewmodel.seeall.tv.SeeAllTvViewModel
@@ -69,6 +72,9 @@ private fun SeeAllTvShowsContent(tvShow: LazyPagingItems<TvShowUiState>) {
             items(tvShow.itemCount) {
                 ItemBasicCard(
                     imagePainter = rememberAsyncImagePainter(model = tvShow[it]!!.seriesImage),
+                    modifier = Modifier
+                        .height(MaterialTheme.dimens.dimens176)
+                        .width(MaterialTheme.dimens.dimens104),
                     hasName = true,
                     name = tvShow[it]!!.seriesName,
                     hasDateAndCountry = true,
