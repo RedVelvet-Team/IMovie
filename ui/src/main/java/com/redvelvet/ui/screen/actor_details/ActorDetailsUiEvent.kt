f3999bbd808b17362169df7b03b4e4967c056256
HassanAyman <hassan3ayman@gmail.com>
2023-08-16 04:21:58 +0300

    delete unused effect and event

--- ui/src/main/java/com/redvelvet/ui/screen/actor_details/ActorDetailsUiEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.redvelvet.ui.screen.actor_details
-
-interface ActorDetailsUiEvent{
-    fun onClickSeeAll()
-    fun onClickReadMore()
-    fun onClickKnownItem(id: String)
-}
--- viewmodels/src/main/java/com/redvelvet/viewmodel/knownFor/ActorWorksUiEffect.kt
+++
@@ -1,7 +0,0 @@
-package com.redvelvet.viewmodel.knownFor
-
-import com.redvelvet.viewmodel.base.BaseUiEffect
-
-sealed interface ActorWorksUiEffect: BaseUiEffect{
-    data object NavigateToItemDetails: ActorWorksUiEffect
-}
