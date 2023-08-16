c324933ca577a7678acdfaa44a304c5521a5f2da
abaferas <wasfyelgamed@gmail.com>
2023-08-16 06:27:26 +0300

    implement home screen view pager

--- ui/src/main/java/com/redvelvet/ui/screen/home/HomeScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/home/HomeScreen.kt
@@ -1,25 +1,35 @@
 package com.redvelvet.ui.screen.home
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
-import com.redvelvet.ui.composable.FilxTabLayout
 import com.redvelvet.ui.composable.FilxTopAppBar
 import com.redvelvet.ui.composable.TabContentDisplay
+import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.color
 import com.redvelvet.ui.theme.dimens
 import com.redvelvet.ui.theme.spacing
@@ -31,21 +41,15 @@ fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val state by viewModel.state.collectAsState()
-    Scaffold(modifier = Modifier.fillMaxSize(),
-        topBar = {
-            FilxTopAppBar(
-                "FlixMovie",
-                hasBackArrow = false
-            )
-        }, bottomBar = {
-        },
-        containerColor = MaterialTheme.color.backgroundPrimary
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        FilxTopAppBar(
+            "FlixMovie", hasBackArrow = false
+        )
+    }, bottomBar = {}, containerColor = MaterialTheme.color.backgroundPrimary
     ) { paddingValues ->
         HomeScreenContent(paddingValues, state)
     }
 }
-
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreenContent(
     paddingValues: PaddingValues, state: HomeUiState
@@ -57,65 +61,130 @@ fun HomeScreenContent(
             )
             .fillMaxWidth()
     ) {
-        val pagerState = rememberPagerState { state.tabLayoutTitles.size }
-        FilxTabLayout(pagerState = pagerState, titles = state.tabLayoutTitles)
-        HorizontalPager(state = pagerState) { currentPage ->
-            AnimatedVisibility(currentPage == 0) {
-                TabContentDisplay(
-                    categories = state.movieCategories,
-                    titles = state.movieCategories.map { movieCategoryUiState -> movieCategoryUiState.title },
-                    imagePainters = state.movieCategories.map { movieCategoryUiState ->
-                        movieCategoryUiState.movies.map { movieUiState ->
-                            rememberAsyncImagePainter(model = movieUiState.movieImage)
+
+        var pagerState by remember { mutableIntStateOf(0) }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MaterialTheme.spacing.spacing16)
+        ) {
+            TabRow(
+                selectedTabIndex = pagerState,
+                containerColor = MaterialTheme.color.backgroundPrimary,
+                indicator = {
+                },
+                divider = {}
+            ) {
+                state.tabLayoutTitles.forEachIndexed { index, title ->
+                    Box {
+                        Tab(
+                            selected = pagerState == index,
+                            onClick = {
+                                pagerState = index
+                            },
+                            text = {
+                                Text(
+                                    text = title,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    color = Color.White,
+                                    style = Typography.headlineSmall
+                                )
+                            })
+                        this@Column.AnimatedVisibility(
+                            visible = pagerState == index,
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth(0.4f)
+                                    .height(MaterialTheme.dimens.dimens4)
+                                    .align(Alignment.BottomCenter)
+                                    .background(
+                                        MaterialTheme.color.brand100,
+                                        RoundedCornerShape(50)
+                                    )
+                            )
                         }
-                    },
-                    hasName = true,
-                    hasDateAndCountry = true,
-                    names = state.movieCategories.map { movieCategoryUiState ->
-                        movieCategoryUiState.movies.map { movieUiState ->
-                            movieUiState.movieName
-                        }
-                    },
-                    dates = state.movieCategories.map { movieCategoryUiState ->
-                        movieCategoryUiState.movies.map { movieUiState ->
-                            movieUiState.movieDate
-                        }
-                    },
-                    countries = state.movieCategories.map { movieCategoryUiState ->
-                        movieCategoryUiState.movies.map { movieUiState ->
-                            movieUiState.countryOfMovie
-                        }
-                    },
-                )
-            }
-            AnimatedVisibility(visible = currentPage != 0) {
-                TabContentDisplay(
-                    categories = state.tvShowCategories,
-                    titles = state.tvShowCategories.map { tvShowCategory -> tvShowCategory.title },
-                    imagePainters = state.tvShowCategories.map { tvShowCategoryUiState ->
-                        tvShowCategoryUiState.tvShows.map { tvShowUiState ->
-                            rememberAsyncImagePainter(model = tvShowUiState.seriesImage)
-                        }
-                    },
-                    hasName = true,
-                    hasDateAndCountry = true,
-                    names = state.tvShowCategories.map { tvShowCategoryUiState ->
-                        tvShowCategoryUiState.tvShows.map { tvShowUiState ->
-                            tvShowUiState.seriesName
-                        }
-                    },
-                    dates = state.tvShowCategories.map { tvShowCategoryUiState ->
-                        tvShowCategoryUiState.tvShows.map { tvShowUiState ->
-                            tvShowUiState.seriesDate
-                        }
-                    },
-                    countries = state.tvShowCategories.map { tvShowCategoryUiState ->
-                        tvShowCategoryUiState.tvShows.map { tvShowUiState ->
-                            tvShowUiState.seriesCountry
-                        }
-                    },
-                )
+                    }
+                }
             }
         }
+        val selectedTabContent: @Composable () -> Unit = when (pagerState) {
+            0 -> {
+                { MovieContent(state = state) }
+            }
+
+            1 -> {
+                { SeriesContent(state = state) }
+            }
+
+            else -> {
+                {}
+            }
+        }
+        selectedTabContent()
     }
 }
+
+
+@Composable
+fun MovieContent(state: HomeUiState) {
+    TabContentDisplay(
+        categories = state.movieCategories,
+        titles = state.movieCategories.map { movieCategoryUiState -> movieCategoryUiState.title },
+        imagePainters = state.movieCategories.map { movieCategoryUiState ->
+            movieCategoryUiState.movies.map { movieUiState ->
+                rememberAsyncImagePainter(model = movieUiState.movieImage)
+            }
+        },
+        hasName = true,
+        hasDateAndCountry = true,
+        names = state.movieCategories.map { movieCategoryUiState ->
+            movieCategoryUiState.movies.map { movieUiState ->
+                movieUiState.movieName
+            }
+        },
+        dates = state.movieCategories.map { movieCategoryUiState ->
+            movieCategoryUiState.movies.map { movieUiState ->
+                movieUiState.movieDate
+            }
+        },
+        countries = state.movieCategories.map { movieCategoryUiState ->
+            movieCategoryUiState.movies.map { movieUiState ->
+                movieUiState.countryOfMovie
+            }
+        },
+    )
+}
+
+@Composable
+fun SeriesContent(state: HomeUiState) {
+    TabContentDisplay(
+        categories = state.tvShowCategories,
+        titles = state.tvShowCategories.map { tvShowCategory -> tvShowCategory.title },
+        imagePainters = state.tvShowCategories.map { tvShowCategoryUiState ->
+            tvShowCategoryUiState.tvShows.map { tvShowUiState ->
+                rememberAsyncImagePainter(model = tvShowUiState.seriesImage)
+            }
+        },
+        hasName = true,
+        hasDateAndCountry = true,
+        names = state.tvShowCategories.map { tvShowCategoryUiState ->
+            tvShowCategoryUiState.tvShows.map { tvShowUiState ->
+                tvShowUiState.seriesName
+            }
+        },
+        dates = state.tvShowCategories.map { tvShowCategoryUiState ->
+            tvShowCategoryUiState.tvShows.map { tvShowUiState ->
+                tvShowUiState.seriesDate
+            }
+        },
+        countries = state.tvShowCategories.map { tvShowCategoryUiState ->
+            tvShowCategoryUiState.tvShows.map { tvShowUiState ->
+                tvShowUiState.seriesCountry
+            }
+        },
+    )
+}
+
