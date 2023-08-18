d167486b639d35e7d0c1b2ba57a723545ea882b4
Kamel Mohmaed Kamel <kikokeko10@gmail.com>
2023-08-18 17:34:22 +0300

    add backend for home

--- ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
+++ ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
@@ -4,18 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import com.redvelvet.ui.LocalNavController
 import com.redvelvet.ui.screen.actor_details.actorDetailsRoute
-import com.redvelvet.ui.screen.forgot_password.forgotPasswordRoute
 import com.redvelvet.ui.screen.episodes.seeAllEpisodeRoute
+import com.redvelvet.ui.screen.forgot_password.forgotPasswordRoute
 import com.redvelvet.ui.screen.home.homeRoute
 import com.redvelvet.ui.screen.known_for.actorKnownForRoute
 import com.redvelvet.ui.screen.login.loginRoute
 import com.redvelvet.ui.screen.movieDetails.movieDetailsRoute
 import com.redvelvet.ui.screen.onboarding.onBoardingRoute
 import com.redvelvet.ui.screen.search.searchRoute
-import com.redvelvet.ui.screen.seeallseasons.seeAllSeasonsRoute
 import com.redvelvet.ui.screen.seeAllMovieImages.seeAllMovieImaesRoute
 import com.redvelvet.ui.screen.seeAllReviews.seeAllMovieReviewsRoute
 import com.redvelvet.ui.screen.seeall.seeAllMovieListRoute
+import com.redvelvet.ui.screen.seeallseasons.seeAllSeasonsRoute
 import com.redvelvet.ui.screen.seealltv.seeAllTvListRoute
 import com.redvelvet.ui.screen.sellAllTopCast.seeAllTopCastRoute
 import com.redvelvet.ui.screen.signup.signupRoute
@@ -28,7 +28,7 @@ import com.redvelvet.ui.screen.upcoming.upcomingRoute
 
 @Composable
 fun MovieNavGraph() {
-    NavHost(LocalNavController.current, startDestination = MovieDestination.Home.route) {
+    NavHost(LocalNavController.current, startDestination = MovieDestination.Splash.route) {
         splashRoute()
         onBoardingRoute()
         actorDetailsRoute()
