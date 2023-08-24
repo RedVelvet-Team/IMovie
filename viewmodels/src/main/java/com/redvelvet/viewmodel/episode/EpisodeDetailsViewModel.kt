51d454f917ec4ee1094e1b5951ba33826d6f348b
abaferas <wasfyelgamed@gmail.com>
2023-08-24 09:07:41 +0300

    implement Episode view model + Args class

--- viewmodels/src/main/java/com/redvelvet/viewmodel/episode/EpisodeDetailsViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/episode/EpisodeDetailsViewModel.kt
@@ -1,298 +1,69 @@
 package com.redvelvet.viewmodel.episode
 
+import androidx.lifecycle.SavedStateHandle
+import com.redvelvet.entities.episode.EpisodeDetails
+import com.redvelvet.usecase.usecase.auth.GetUserSessionIdUseCase
+import com.redvelvet.usecase.usecase.episode.GetEpisodeDetailsUseCase
+import com.redvelvet.viewmodel.base.BaseViewModel
+import com.redvelvet.viewmodel.base.ErrorUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class EpisodeDetailsViewModel {
-    val fakeEpisodeDetails = EpisodeDetails(
-        episodeDetails = EpisodeDetails.EpisodeDetails(
-            id = 1,
-            name = "The Good Doctor",
-            overview = "Shaun Murphy, a young surgeon with autism and savant syndrome, relocates from a quiet country life to join a prestigious hospital's surgical unit. Unable to personally connect with those around him...More",
-            runtime = 45,
-            seasonNumber = 2,
-            episodeNumber = 5,
-            airDate = "2023-08-24",
-            stillPath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-            voteCount = 100,
-            voteAverage = 8.5
-        ),
-        episodeMovies = EpisodeDetails.EpisodeMovies(
-            id = 1,
-            results = listOf(
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-                EpisodeDetails.EpisodeMovies.Result(
-                    id = "1",
-                    name = "Movie 1",
-                    key = "movie1key",
-                    site = "YouTube",
-                    size = 720,
-                    type = "Trailer",
-                    official = true,
-                    publishedAt = "2023-08-25"
-                ),
-            )
-        ),
-        episodeAccountStatus = EpisodeDetails.EpisodeAccountStatus(
-            id = 1,
-            rated = EpisodeDetails.EpisodeAccountStatus.Rated(9.0)
-        ),
-        episodeCast = EpisodeDetails.EpisodeCast(
-            cast = listOf(
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeCast.Cast(
-                    id = 101,
-                    name = "Actor 1",
-                    profilePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-            )
-        ),
-        episodeImages = EpisodeDetails.EpisodeImages(
-            id = 1,
-            stills = listOf(
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-                ),
-                EpisodeDetails.EpisodeImages.Still(
-                    filePath = "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+@HiltViewModel
+class EpisodeDetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val getEpisodeDetailsUseCase: GetEpisodeDetailsUseCase,
+    private val getUserSessionIdUseCase: GetUserSessionIdUseCase
+) : BaseViewModel<EpisodeDetailsUiState, EpisodeDetailsUiEffect>(EpisodeDetailsUiState()) {
+
+    private val args: EpisodeDetailsArgs = EpisodeDetailsArgs(savedStateHandle = savedStateHandle)
+
+    init {
+        getData()
+    }
+
+    private fun getData() {
+//        tryToExecute(
+//            execute = { getEpisodeDetailsUseCase(
+//                tvId = args.tvId,
+//                seasonNumber = args.seasonId,
+//                episodeNumber = args.episodeId,
+//                sessionId = "c6318ed5d979bdec77cbb027a0cbaa3ef8f8df34"//getUserSessionIdUseCase()
+//            ) },
+//            onSuccessWithData = ::onSuccess,
+//            onError = ::onError,
+//        )
+        tryToExecute(
+            execute = {
+                getEpisodeDetailsUseCase(
+                    tvId = 1396,
+                    seasonNumber = 1,
+                    episodeNumber = 1,
+                    sessionId = "c6318ed5d979bdec77cbb027a0cbaa3ef8f8df34"
                 )
+            },
+            onSuccessWithData = ::onSuccess,
+            onError = ::onError,
+        )
+    }
+
+    private fun onSuccess(episodeDetails: EpisodeDetails) {
+        _state.update {
+            EpisodeDetailsUiState(
+                data = episodeDetails.toUiState(),
+                isLoading = false,
+                isError = null,
             )
-        )
-    )
-
-}
-
-
-data class EpisodeDetails(
-    val episodeDetails: EpisodeDetails,
-    val episodeMovies: EpisodeMovies,
-    val episodeAccountStatus: EpisodeAccountStatus,
-    val episodeCast: EpisodeCast,
-    val episodeImages: EpisodeImages
-) {
-    data class EpisodeDetails(
-        val id: Int,
-        val name: String,
-        val overview: String,
-        val runtime: Int,
-        val seasonNumber: Int,
-        val episodeNumber: Int,
-        val airDate: String,
-        val stillPath: String,
-        val voteCount: Int,
-        val voteAverage: Double
-    )
-
-    data class EpisodeMovies(
-        val id: Int,
-        val results: List<Result>
-    ) {
-        data class Result(
-            val id: String,
-            val name: String,
-            val key: String,
-            val site: String,
-            val size: Int,
-            val type: String,
-            val official: Boolean,
-            val publishedAt: String,
-        )
+        }
     }
 
-    data class EpisodeAccountStatus(
-        val id: Int,
-        val rated: Rated
-    ) {
-        data class Rated(
-            val value: Double
-        )
-    }
-
-    data class EpisodeCast(
-        val cast: List<Cast>,
-    ) {
-        data class Cast(
-            val id: Int,
-            val name: String,
-            val profilePath: String
-        )
-    }
-
-    data class EpisodeImages(
-        val id: Int,
-        val stills: List<Still>
-    ) {
-        data class Still(
-            val filePath: String,
-        )
+    private fun onError(error: ErrorUiState) {
+        _state.update {
+            it.copy(
+                isLoading = false,
+                isError = error,
+            )
+        }
     }
 }
