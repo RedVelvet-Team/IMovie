8fed006a8339775347cba9fa30983e4eaa11d0e5
abaferas <wasfyelgamed@gmail.com>
2023-08-25 07:41:30 +0300

    implement  interaction Listener

--- viewmodels/src/main/java/com/redvelvet/viewmodel/episode/EpisodeDetailsInteraction.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/episode/EpisodeDetailsInteraction.kt
@@ -1,4 +1,13 @@
 package com.redvelvet.viewmodel.episode
 
 interface EpisodeDetailsInteraction {
+    fun onClickBack()
+    fun onClickFavorite(episodeID: Int)
+    fun onClickSave(episodeID: Int)
+    fun onClickTopCastSeeAll(topCastId: Int)
+    fun onCLickImagesSeeAll(imagesId: Int)
+    fun onClickCast(castId: Int)
+    fun onClickVideo(videoKey: String)
+    fun onCLickRefresh()
+    fun onLoginRequired()
 }
