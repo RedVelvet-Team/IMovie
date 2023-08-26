864e9040fd297987112197f9623680fb6d92faa2
HassanAyman <hassan3ayman@gmail.com>
2023-08-27 00:40:44 +0300

    add uistate and the viewmodel for the gameScreen

--- viewmodels/src/main/java/com/redvelvet/viewmodel/game/GameUiState.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/game/GameUiState.kt
@@ -1,19 +1,29 @@
 package com.redvelvet.viewmodel.game
 
+import com.redvelvet.entities.Question
 import com.redvelvet.viewmodel.base.BaseUiState
 import com.redvelvet.viewmodel.base.ErrorUiState
 
 data class GameUiState(
-    val number: String = "",
-    val question: String = "",
-    val answers: List<AnswerUiState> = emptyList(),
-    val questionScore: String = "",
     val isLoading: Boolean = true,
-    val error: ErrorUiState? = null
+    val error: ErrorUiState? = null,
+    val question: QuestionUiState = QuestionUiState()
 ): BaseUiState
 
 
+data class QuestionUiState(
+    val number: Int = 0,
+    val question: String = "",
+    val answers: List<AnswerUiState> = emptyList(),
+    val questionScore: String = "",
+)
 data class AnswerUiState(
     val isCorrect: Boolean = false,
     val text: String = ""
+)
+
+fun Question.toUiState() = QuestionUiState(
+    number = 0,
+    question = this.question,
+    answers = this.answers.map { AnswerUiState(text = it) }
 )
--- viewmodels/src/main/java/com/redvelvet/viewmodel/game/GameViewModel.kt
+++ viewmodels/src/main/java/com/redvelvet/viewmodel/game/GameViewModel.kt
@@ -1,12 +1,50 @@
 package com.redvelvet.viewmodel.game
 
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import androidx.paging.LOGGER
+import com.redvelvet.entities.Question
+import com.redvelvet.usecase.usecase.GetQuestionUseCase
 import com.redvelvet.viewmodel.base.BaseViewModel
+import com.redvelvet.viewmodel.base.ErrorUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GameViewModel @Inject constructor(
+    private val getQuestion: GetQuestionUseCase
+) : BaseViewModel<GameUiState, Unit>(GameUiState()) {
 
-): BaseViewModel<GameUiState, Unit>(GameUiState()) {
+    private var questions = listOf<Pair<String, List<String>>>()
 
+    init {
+        getQuestion()
+    }
+
+    private fun getQuestion() {
+        viewModelScope.launch {
+            delay(1000)
+            tryToExecute(
+                execute = { getQuestion.getMovieQuestion() },
+                onSuccessWithData = ::onGetQuestionSuccess,
+                onError = ::onError
+            )
+        }
+    }
+
+    private fun onGetQuestionSuccess(question: Question) {
+        _state.update {
+            it.copy(isLoading = false, question = question.toUiState()) }
+    }
+
+    private fun onError(error: ErrorUiState) {
+        _state.update { it.copy(isLoading = false, error = error) }
+    }
+
+    fun onClickAnswer(answer: AnswerUiState): Boolean {
+       return getQuestion.isCorrectAnswer(answer.text).also { getQuestion() }
+    }
 }
