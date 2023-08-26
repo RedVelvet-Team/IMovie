56575c0d297cf13fda4e06c5e72fb8832a39d7f4
HassanAyman <hassan3ayman@gmail.com>
2023-08-27 00:40:02 +0300

    add getQuestionUseCase that get the question of every type and check if the question is correct

---
+++ usecase/src/main/java/com/redvelvet/usecase/usecase/GetQuestionUseCase.kt
@@ -0,0 +1,34 @@
+package com.redvelvet.usecase.usecase
+
+import com.redvelvet.entities.Question
+import com.redvelvet.usecase.repository.MovieRepository
+import javax.inject.Inject
+
+class GetQuestionUseCase @Inject constructor(
+   private val repository: MovieRepository
+) {
+    private var questions = listOf<Question>()
+    private var questionIndex = 0
+
+    suspend fun getMovieQuestion(): Question{
+        if (questions.isEmpty()){
+            questions = repository.getMovieQuestions()
+        }
+        return questions[questionIndex].also { questionIndex++ }
+    }
+
+    suspend fun getTvQuestion(): Question{
+        if (questions.isEmpty()){
+            questions = repository.getTvQuestions()
+        }
+        return questions[questionIndex].also { questionIndex++ }
+    }
+    suspend fun getActingQuestion(): Question{
+        if (questions.isEmpty()){
+            questions = repository.getActingQuestions()
+        }
+        return questions[questionIndex].also { questionIndex++ }
+    }
+
+    fun isCorrectAnswer(answer: String) = questions[questionIndex].correctAnswer == answer
+}
