3da46e11c2ed8c43624c960d139de1fb179747c7
Narges-Nagy <nargesnagy21@gmail.com>
2023-08-27 03:06:50 +0300

    add create list repository injection

--- app/src/main/java/com/redvelvet/imovie/di/RepositoryModule.kt
+++ app/src/main/java/com/redvelvet/imovie/di/RepositoryModule.kt
@@ -1,17 +1,7 @@
 package com.redvelvet.imovie.di
 
-import com.redvelvet.repository.repository.AuthRepositoryImpl
-import com.redvelvet.repository.repository.LibraryRepositoryImpl
-import com.redvelvet.repository.repository.MediaActionsRepositoryImpl
-import com.redvelvet.repository.repository.MovieRepositoryImpl
-import com.redvelvet.repository.repository.TvShowRepositoryImp
-import com.redvelvet.repository.repository.UserRepositoryImpl
-import com.redvelvet.usecase.repository.AuthRepository
-import com.redvelvet.usecase.repository.LibraryRepository
-import com.redvelvet.usecase.repository.MediaActionsRepository
-import com.redvelvet.usecase.repository.MovieRepository
-import com.redvelvet.usecase.repository.TvShowRepository
-import com.redvelvet.usecase.repository.UserRepository
+import com.redvelvet.repository.repository.*
+import com.redvelvet.usecase.repository.*
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -56,5 +46,13 @@ abstract class RepositoryModule {
     abstract fun bindLibraryRepository(
         libraryRepository: LibraryRepositoryImpl
     ): LibraryRepository
+
+
+    @Singleton
+    @Binds
+    abstract fun bindLibraryListRepository(
+        libraryListRepository: LiberaryListRepositoryImp
+    ): LiberaryListsRepository
+
 
 }
