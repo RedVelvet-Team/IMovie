52dd746210f7166083cda123bffab44313915aae
HassanAyman <hassan3ayman@gmail.com>
2023-08-28 17:01:20 +0300

    add the question number slider in the top of the screen and update the state of the question index

--- ui/src/main/java/com/redvelvet/ui/screen/game/GameScreen.kt
+++ ui/src/main/java/com/redvelvet/ui/screen/game/GameScreen.kt
@@ -1,7 +1,9 @@
 package com.redvelvet.ui.screen.game
 
+import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
@@ -13,6 +15,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,6 +23,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -31,10 +38,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
@@ -45,6 +52,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.redvelvet.ui.R
 import com.redvelvet.ui.composable.FlixMovieScaffold
+import com.redvelvet.ui.theme.FontAccent
+import com.redvelvet.ui.theme.OnSecondary
+import com.redvelvet.ui.theme.Primary
 import com.redvelvet.ui.theme.Secondary
 import com.redvelvet.ui.theme.SecondaryCard2
 import com.redvelvet.ui.theme.SecondaryCard3
@@ -52,8 +62,11 @@ import com.redvelvet.ui.theme.Typography
 import com.redvelvet.ui.theme.color
 import com.redvelvet.ui.theme.spacing
 import com.redvelvet.viewmodel.game.AnswerUiState
+import com.redvelvet.viewmodel.game.Correctness
 import com.redvelvet.viewmodel.game.GameUiState
 import com.redvelvet.viewmodel.game.GameViewModel
+import com.redvelvet.viewmodel.game.QuestionNumberState
+import kotlinx.coroutines.launch
 
 @Composable
 fun GameScreen(
@@ -62,10 +75,9 @@ fun GameScreen(
     val state by viewModel.state.collectAsState()
 
     FlixMovieScaffold(
-        isLoading = state.isLoading,
-        error = state.error
+        isLoading = state.isLoading, error = state.error
     ) {
-        if (state.isGameFinished){
+        if (state.isGameFinished) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -73,80 +85,171 @@ fun GameScreen(
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Finished", color = Color.White, style = MaterialTheme.typography.headlineLarge)
+                Text(
+                    text = "Finished",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineLarge
+                )
+            }
+        } else {
+            GameContent(
+                state = state, onClick = viewModel::onClickAnswer
+            )
+        }
+    }
+}
+
+@SuppressLint("CoroutineCreationDuringComposition")
+@Composable
+fun GameContent(state: GameUiState, onClick: (AnswerUiState) -> Boolean) {
+    val questionSTate = rememberLazyListState()
+    val coroutineScore = rememberCoroutineScope()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        coroutineScore.launch {
+            questionSTate.animateScrollToItem(state.currentQuestionNumber)
+        }
+        DividerRow(number = 24)
+        LazyRow(
+            contentPadding = PaddingValues(8.dp), horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.number) {
+                QuestionNumber(question = it)
             }
         }
-        GameContent(
-            state = state,
-            onClick = viewModel::onClickAnswer
+        DividerRow(number = 24)
+        Spacer(modifier = Modifier.height(70.dp))
+        RepeatableCardStack(
+            state = state, onClick = onClick
         )
     }
 }
 
+
 @Composable
-fun GameContent(state: GameUiState, onClick: (AnswerUiState) -> Boolean) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
+fun QuestionNumber(
+    question: QuestionNumberState
+) {
+    val boxBackground by animateColorAsState(
+        targetValue = when (question.correctness) {
+            Correctness.NOT_ANSWERED -> OnSecondary
+            Correctness.CORRECT -> Secondary
+            Correctness.WRONG -> Secondary
+            Correctness.CURRENT_ANSWERED -> Color(0xFF6C5DD3)
+        }, label = ""
+    )
+
+    val textColor by animateColorAsState(
+        targetValue = when (question.correctness) {
+            Correctness.CURRENT_ANSWERED -> MaterialTheme.color.fontPrimary
+            Correctness.NOT_ANSWERED -> MaterialTheme.color.fontAccent
+            else -> Color.Transparent
+        }, label = ""
+    )
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(boxBackground)
+            .padding(8.dp)
     ) {
-        RepeatableCardStack(
-            state = state,
-            onClick = onClick
-        )
+        AnimatedVisibility(
+            visible = question.correctness == Correctness.CORRECT,
+            enter = fadeIn(tween(100))
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(Color(0x80FFFFFF))
+                    .padding(5.dp),
+                painter = painterResource(id = R.drawable.ic_correct),
+                contentDescription = "",
+                tint = MaterialTheme.color.fontAccent
+            )
+        }
+        AnimatedVisibility(
+            visible = question.correctness == Correctness.WRONG,
+            enter = fadeIn(tween(100))
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(Color(0x80BC4343))
+                    .padding(5.dp),
+                painter = painterResource(id = R.drawable.ic_wrong),
+                contentDescription = "",
+                tint = Color(0xFFBC4343)
+            )
+        }
+        AnimatedVisibility(
+            visible = question.correctness == Correctness.NOT_ANSWERED || question.correctness == Correctness.CURRENT_ANSWERED,
+            enter = fadeIn(tween(100)),
+            exit = fadeOut(tween(100))
+        ) {
+            Text(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(Color.Transparent),
+                text = question.number.toString(),
+                color = textColor,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun RepeatableCardStack(state: GameUiState, onClick: (AnswerUiState) -> Boolean) {
-
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 16.dp, end = 16.dp, top = 32.dp)
             .height(150.dp)
     ) {
-        repeat(3) { index ->
-            val cardModifier = Modifier
-                .height(120.dp)
-                .rotate(
-                    when (2 - index) {
-                        0 -> 0f
-                        1 -> 5f
-                        2 -> -5f
-                        else -> -5f
-                    }
-                )
-                .alpha(if (2 - index == 0) 1f else .2f)
-                .align(Alignment.Center)
 
-            Card(
-                modifier = cardModifier,
-                colors = CardDefaults.cardColors(
-                    containerColor = when (2 - index) {
-                        0 -> Secondary
-                        1 -> SecondaryCard2
-                        3 -> SecondaryCard3
-                        else -> Color.Green
-                    }
-                )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .rotate(5f)
+                .align(Alignment.Center)
+                .clip(RoundedCornerShape(8.dp))
+                .background(SecondaryCard3)
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .rotate(-5f)
+                .align(Alignment.Center)
+                .clip(RoundedCornerShape(8.dp))
+                .background(SecondaryCard2)
+        )
+        Card(
+            modifier = Modifier
+                .height(120.dp)
+                .align(Alignment.Center),
+            colors = CardDefaults.cardColors(Secondary),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            AnimatedContent(
+                targetState = state.question, transitionSpec = {
+                    fadeIn(tween(1000)) togetherWith fadeOut(tween(100))
+                }, label = ""
             ) {
-                AnimatedContent(
-                    targetState = state.question,
-                    transitionSpec = {
-                        fadeIn(tween(1000)) togetherWith fadeOut(tween(100))
-                    }, label = ""
+                Box(
+                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                 ) {
-                    Box(
-                        modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = it.question,
-                            textAlign = TextAlign.Center,
-                            style = MaterialTheme.typography.headlineMedium,
-                            color = MaterialTheme.color.fontPrimary
-                        )
-                    }
+                    Text(
+                        text = it.question,
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineMedium,
+                        color = MaterialTheme.color.fontPrimary
+                    )
                 }
             }
         }
@@ -167,17 +270,17 @@ fun RepeatableCardStack(state: GameUiState, onClick: (AnswerUiState) -> Boolean)
                 tint = Color.White
             )
             Text(
-                text = state.score,
-                style = Typography.bodySmall,
-                color = Color.Black
+                text = state.score, style = Typography.bodySmall, color = Color.Black
             )
         }
     }
 
     Spacer(modifier = Modifier.height(60.dp))
 
-    repeat(state.question.answers.size) {index ->
-        Answer(answer = state.question.answers[index], onClick = onClick, isAnswered = state.isAnswered)
+    repeat(state.question.answers.size) { index ->
+        Answer(
+            answer = state.question.answers[index], onClick = onClick, isAnswered = state.isAnswered
+        )
     }
 
 }
@@ -191,39 +294,33 @@ fun Answer(answer: AnswerUiState, onClick: (AnswerUiState) -> Boolean, isAnswere
 
     val context = LocalContext.current
     val backgroundColor by animateColorAsState(
-        targetValue = if (answer.text == isSelected) Color.Green else Secondary,
-        label = "")
+        targetValue = if (answer.text == isSelected) Color.Green else Secondary, label = ""
+    )
     val modifier = Modifier
         .fillMaxWidth()
         .padding(
-            horizontal = MaterialTheme.spacing.spacing16,
-            vertical = MaterialTheme.spacing.spacing4
+            horizontal = MaterialTheme.spacing.spacing16, vertical = MaterialTheme.spacing.spacing4
         )
         .clip(RoundedCornerShape(16.dp))
         .background(backgroundColor)
 
-    Row(
-        modifier = if (!isAnswered){
-            modifier
-                .clickable {
-                    isSelected = answer.text
-                    val isCorrect = onClick(answer)
-                    Toast
-                        .makeText(
-                            context,
-                            if (isCorrect) "Correct" else "false",
-                            Toast.LENGTH_SHORT
-                        )
-                        .show()
-                }
-                .padding(vertical = MaterialTheme.spacing.spacing16)
-        } else modifier.padding(vertical = MaterialTheme.spacing.spacing16) ,
+    Row(modifier = if (!isAnswered) {
+        modifier
+            .clickable {
+                isSelected = answer.text
+                val isCorrect = onClick(answer)
+                Toast
+                    .makeText(
+                        context, if (isCorrect) "Correct" else "false", Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+            .padding(vertical = MaterialTheme.spacing.spacing16)
+    } else modifier.padding(vertical = MaterialTheme.spacing.spacing16),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
+        horizontalArrangement = Arrangement.Center) {
         AnimatedContent(
-            targetState = answer,
-            transitionSpec = {
+            targetState = answer, transitionSpec = {
                 fadeIn(tween(1000)) togetherWith fadeOut(tween(100))
             }, label = ""
         ) {
@@ -235,4 +332,31 @@ fun Answer(answer: AnswerUiState, onClick: (AnswerUiState) -> Boolean, isAnswere
             )
         }
     }
+}
+
+@Composable
+fun DividerRow(
+    number: Int, modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        repeat(number) {
+            Spacer(
+                modifier = Modifier
+                    .width(12.dp)
+                    .height(4.dp)
+                    .clip(RoundedCornerShape(1.dp))
+                    .background(Color(0x4DD9D9D9))
+            )
+        }
+    }
+}
+
+@Composable
+fun QuestionPreview() {
+
 }
