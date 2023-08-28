7c69d295d47490fa1f16cab4b18bd724196bd30d
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2023-08-29 01:16:50 +0300

    implement nav graph and category route

--- ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
+++ ui/src/main/java/com/redvelvet/ui/navigation/MovieNavGraph.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import com.redvelvet.ui.LocalNavController
 import com.redvelvet.ui.screen.actor_details.actorDetailsRoute
+import com.redvelvet.ui.screen.category.categoryRoute
 import com.redvelvet.ui.screen.episodes.seeAllEpisodeRoute
 import com.redvelvet.ui.screen.forgot_password.forgotPasswordRoute
 import com.redvelvet.ui.screen.home.homeRoute
@@ -18,20 +19,22 @@ import com.redvelvet.ui.screen.search.searchRoute
 import com.redvelvet.ui.screen.seeAllMovieImages.seeAllMovieImaesRoute
 import com.redvelvet.ui.screen.seeAllReviews.seeAllMovieReviewsRoute
 import com.redvelvet.ui.screen.seeall.seeAllMovieListRoute
-import com.redvelvet.ui.screen.seealltv.seeAllTvListRoute
 import com.redvelvet.ui.screen.seeallseasons.seeAllSeasonsRoute
+import com.redvelvet.ui.screen.seealltv.seeAllTvListRoute
 import com.redvelvet.ui.screen.sellAllTopCast.seeAllTopCastRoute
 import com.redvelvet.ui.screen.signup.signupRoute
 import com.redvelvet.ui.screen.splash.splashRoute
 import com.redvelvet.ui.screen.tvshowdetails.tvShowDetailsRoute
-import com.redvelvet.ui.screen.upcoming.categoryRoute
 import com.redvelvet.ui.screen.upcoming.gameRoute
 import com.redvelvet.ui.screen.upcoming.upcomingRoute
 import com.redvelvet.ui.screen.youtube_player.youtubePlayerRoute
 
 @Composable
 fun MovieNavGraph() {
-    NavHost(LocalNavController.current, startDestination = MovieDestination.Splash.route) {
+    NavHost(
+        LocalNavController.current,
+        startDestination = MovieDestination.Category.route
+    ) {
         splashRoute()
         onBoardingRoute()
         actorDetailsRoute()
